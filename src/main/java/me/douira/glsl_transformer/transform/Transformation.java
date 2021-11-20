d78c1cd3cd4003bc72bda62d87d511b915204c8a
douira <douira100@gmail.com>
2021-11-20 22:48:53 +0100

    added semantic exception for issues noticed during transformation

--- src/main/java/me/douira/glsl_transformer/transform/Transformation.java
+++ src/main/java/me/douira/glsl_transformer/transform/Transformation.java
@@ -1,6 +1,24 @@
 package me.douira.glsl_transformer.transform;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+
 public abstract class Transformation {
+  public class SemanticException extends RuntimeException {
+    public ParserRuleContext node;
+
+    public SemanticException() {
+    }
+    
+    public SemanticException(String message) {
+      super(message);
+    }
+
+    public SemanticException(String message, ParserRuleContext node) {
+      this(message);
+      this.node = node;
+    }
+  }
+
   private PhaseCollector collector;
   private int phaseCounter = 0;
 
