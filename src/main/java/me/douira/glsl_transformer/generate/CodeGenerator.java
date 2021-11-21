614db5e6e45674b1f9913db03640a9a5d566c0c4
douira <douira100@gmail.com>
2021-11-21 01:06:33 +0100

    start generator code

---
+++ src/main/java/me/douira/glsl_transformer/generate/CodeGenerator.java
@@ -0,0 +1,12 @@
+package me.douira.glsl_transformer.generate;
+
+public abstract class CodeGenerator {
+  @Override
+  public String toString() {
+    var builder = new StringBuilder();
+
+    // TODO: collect fragments
+
+    return builder.toString();
+  }
+}
