05f95f671bb5cc292b602b256718a57ae9687115
douira <douira100@gmail.com>
2021-12-23 19:50:56 +0100

    refactor test resource loading

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/IntegratedTest.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/IntegratedTest.java
@@ -2,13 +2,6 @@ package io.github.douira.glsl_transformer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
-
-import io.github.douira.glsl_transformer.GLSLParser.TranslationUnitContext;
-import io.github.douira.glsl_transformer.transform.PhaseCollector;
-import io.github.douira.glsl_transformer.transform.Transformation;
-import io.github.douira.glsl_transformer.transform.TransformationPhase;
-
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -17,17 +10,24 @@ import org.antlr.v4.runtime.BufferedTokenStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.jupiter.api.BeforeEach;
+
+import io.github.douira.glsl_transformer.GLSLParser.TranslationUnitContext;
+import io.github.douira.glsl_transformer.transform.PhaseCollector;
+import io.github.douira.glsl_transformer.transform.Transformation;
+import io.github.douira.glsl_transformer.transform.TransformationPhase;
 
 public abstract class IntegratedTest {
-  public static String testCode;
+  public String testCode;
   public CharStream input;
   public GLSLLexer lexer;
   public GLSLParser parser;
   public BufferedTokenStream tokenStream;
   public TranslationUnitContext tree;
 
+  public static String testResourceInput;
   public static void readInput(Class<?> caller) {
-    testCode = assertDoesNotThrow(
+    testResourceInput = assertDoesNotThrow(
         () -> Files.readString(Paths.get(caller.getResource("/"
             + caller.getSimpleName() + ".glsl").toURI()),
             StandardCharsets.UTF_8));
@@ -35,16 +35,16 @@ public abstract class IntegratedTest {
 
   @BeforeEach
   public void setupParsing() {
+    setupParsingWith(testResourceInput);
+  }
+
+  public void setupParsingWith(String code) {
+    testCode = code;
     input = CharStreams.fromString(testCode);
     lexer = new GLSLLexer(input);
     tokenStream = new CommonTokenStream(lexer);
     parser = new GLSLParser(tokenStream);
     tree = parser.translationUnit();
-  }
-
-  public void setupParsingWith(String code) {
-    testCode = code;
-    setupParsing();
   }
 
   public void wrapRunTransform(TransformationPhase phase) {
