63741c9d1f6941843619da6bdc55af5f820daa3c
douira <douira100@gmail.com>
2021-12-23 19:51:18 +0100

    test case caching, added snapshots and cases

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/TestCaseProvider.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/TestCaseProvider.java
@@ -1,8 +1,15 @@
 package io.github.douira.glsl_transformer;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.stream.Stream;
 
@@ -13,39 +20,56 @@ import org.junit.jupiter.params.provider.ArgumentsProvider;
 import au.com.origin.snapshots.annotations.SnapshotName;
 
 public class TestCaseProvider implements ArgumentsProvider {
-  @Override
-  public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
-    // TODO: read config from snapshot properties
-    // find the test file and read the corresponding testcase file
+  private record TestCase(String snapshotName, String scenario, String content) {
+  };
 
-    var fileUnderTest = Paths.get(context.getRequiredTestClass().getName()
-        .replaceAll("\\.", Matcher.quoteReplacement(File.separator)));
-    var contents = Files.readString(
-        Paths.get("src", "test", "java").resolve(
-            fileUnderTest.getParent()
-                .resolve("__snapshots__")
-                .resolve(fileUnderTest.getFileName() + ".cases")));
+  static final Map<Path, List<TestCase>> testCaseCache = new HashMap<>();
 
-    // parse the testcases
-    var snapshotName = context.getRequiredTestMethod().getAnnotation(SnapshotName.class).value();
-    var rawCases = contents.trim().split("§");
-    var testCases = Stream.<Arguments>builder();
-    for (var i = 1; i < rawCases.length; i++) {
-      var rawCase = rawCases[i];
-      var segments = rawCase.split(" |:", 3);
-      var testCaseSnapshotName = segments[0];
-      var scenario = segments[1];
-      var testCase = segments[2].trim();
+  private List<TestCase> getTestCases(Path file) throws IOException {
+    var testCases = testCaseCache.get(file);
+    if (testCases == null) {
+      var contents = Files.readString(file);
 
-      // only collect test cases relevant to this snapshot
-      // TODO: caching of the loaded file for other tests
-      if (!testCaseSnapshotName.equals(snapshotName)) {
-        continue;
+      // parse the testcases
+      var rawCases = contents.trim().split("§");
+      testCases = new ArrayList<>(rawCases.length - 1);
+      for (var i = 1; i < rawCases.length; i++) {
+        var rawCase = rawCases[i];
+        var segments = rawCase.split(" |:", 3);
+        testCases.add(new TestCase(
+            segments[0], segments[1], segments[2].trim()));
       }
 
-      testCases.accept(Arguments.of(scenario, testCase));
+      testCaseCache.put(file, testCases);
     }
 
-    return testCases.build();
+    return testCases;
+  }
+
+  @Override
+  public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+    // TODO: read snapshot files config from snapshot properties
+    var fileUnderTest = Paths.get(context.getRequiredTestClass().getName()
+        .replaceAll("\\.", Matcher.quoteReplacement(File.separator)));
+    var testCaseFile = Paths.get("src", "test", "java").resolve(
+        fileUnderTest.getParent()
+            .resolve("__snapshots__")
+            .resolve(fileUnderTest.getFileName() + ".cases"));
+
+    var testCases = getTestCases(testCaseFile);
+
+    // get the name of the snapshot from the method or the annotation
+    var snapshotName = Optional.ofNullable(
+        context
+            .getRequiredTestMethod().getAnnotation(SnapshotName.class))
+        .map(SnapshotName::value)
+        .orElseGet(
+            () -> context.getRequiredTestMethod().getName());
+
+    // filter and map the test cases into an argument stream
+    return testCases
+        .stream()
+        .filter(testCase -> testCase.snapshotName().equals(snapshotName))
+        .map(testCase -> Arguments.of(testCase.scenario(), testCase.content()));
   }
 }
