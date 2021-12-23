309ab070e22667f96ebb8f4a62204e5f83d264f8
douira <douira100@gmail.com>
2021-12-23 04:21:35 +0100

    snapshot tests work with testcase loader

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/TestCaseProvider.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/TestCaseProvider.java
@@ -1,5 +1,9 @@
 package io.github.douira.glsl_transformer;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -11,10 +15,37 @@ import au.com.origin.snapshots.annotations.SnapshotName;
 public class TestCaseProvider implements ArgumentsProvider {
   @Override
   public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+    // TODO: read config from snapshot properties
+    // find the test file and read the corresponding testcase file
+
+    var fileUnderTest = Paths.get(context.getRequiredTestClass().getName()
+        .replaceAll("\\.", Matcher.quoteReplacement(File.separator)));
+    var contents = Files.readString(
+        Paths.get("src", "test", "java").resolve(
+            fileUnderTest.getParent()
+                .resolve("__snapshots__")
+                .resolve(fileUnderTest.getFileName() + ".cases")));
+
+    // parse the testcases
     var snapshotName = context.getRequiredTestMethod().getAnnotation(SnapshotName.class).value();
-    return Stream.of(
-        Arguments.of("empty", ""),
-        Arguments.of("simple", "#version 5432\nbar;"),
-        Arguments.of("2", "bar;"));
+    var rawCases = contents.trim().split("§");
+    var testCases = Stream.<Arguments>builder();
+    for (var i = 1; i < rawCases.length; i++) {
+      var rawCase = rawCases[i];
+      var segments = rawCase.split(" |:", 3);
+      var testCaseSnapshotName = segments[0];
+      var scenario = segments[1];
+      var testCase = segments[2].trim();
+
+      // only collect test cases relevant to this snapshot
+      // TODO: caching of the loaded file for other tests
+      if (!testCaseSnapshotName.equals(snapshotName)) {
+        continue;
+      }
+
+      testCases.accept(Arguments.of(scenario, testCase));
+    }
+
+    return testCases.build();
   }
 }
