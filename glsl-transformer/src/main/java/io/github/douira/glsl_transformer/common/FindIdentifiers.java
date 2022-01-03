6fb6a2a5426ac3e9cab5613a9c838c094c78c4c5
douira <douira100@gmail.com>
2022-01-03 04:08:52 +0100

    added throw on matching identifiers phase

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/common/FindIdentifiers.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/common/FindIdentifiers.java
@@ -1,7 +1,7 @@
 package io.github.douira.glsl_transformer.common;
 
 import java.util.Collection;
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.TerminalNode;
@@ -10,7 +10,7 @@ import io.github.douira.glsl_transformer.GLSLLexer;
 import io.github.douira.glsl_transformer.transform.WalkPhase;
 
 public class FindIdentifiers extends WalkPhase {
-  public record Target(String contained, Consumer<Token> action) {
+  public record Target(String contained, BiConsumer<TerminalNode, Token> action) {
   }
 
   private Collection<Target> targets;
@@ -26,7 +26,7 @@ public class FindIdentifiers extends WalkPhase {
       String text = token.getText();
       for (var target : targets) {
         if (text.contains(target.contained())) {
-          target.action().accept(token);
+          target.action().accept(node, token);
         }
       }
     }
---
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/common/ThrowIdentifiers.java
@@ -0,0 +1,22 @@
+package io.github.douira.glsl_transformer.common;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import io.github.douira.glsl_transformer.transform.SemanticException;
+
+public class ThrowIdentifiers extends FindIdentifiers {
+  public record IllegalTarget(String contained, String message) {
+  }
+
+  public ThrowIdentifiers(Collection<IllegalTarget> targets) {
+    super(targets.stream()
+        .map(
+            target -> new Target(
+                target.contained(),
+                (node, token) -> {
+                  throw new SemanticException(target.message(), node);
+                }))
+        .collect(Collectors.toList()));
+  }
+}
