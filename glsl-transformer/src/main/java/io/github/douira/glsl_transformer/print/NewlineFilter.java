55f561aa92718911cfd8da4e201dba6580e11c51
douira <douira100@gmail.com>
2022-01-08 19:38:28 +0100

    added newline filter

---
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/print/NewlineFilter.java
@@ -0,0 +1,26 @@
+package io.github.douira.glsl_transformer.print;
+
+import org.antlr.v4.runtime.Token;
+
+import io.github.douira.glsl_transformer.GLSLLexer;
+
+/**
+ * The trim filter filters out regular unnecessary newlines if there is more
+ * than one.
+ */
+public class NewlineFilter implements TokenFilter {
+  private boolean lastWasNewline;
+
+  @Override
+  public void resetState() {
+    lastWasNewline = false;
+  }
+
+  @Override
+  public boolean isTokenAllowed(Token token) {
+    var isNewline = token.getType() == GLSLLexer.EOL;
+    var allowToken = !(lastWasNewline && isNewline);
+    lastWasNewline = isNewline;
+    return allowToken;
+  }
+}
