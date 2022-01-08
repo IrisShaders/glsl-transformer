ee6b6be806843a352d657194f184ebcb8b23c09e
douira <douira100@gmail.com>
2022-01-08 21:09:00 +0100

    improve channel handling and init

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/print/TokenFilter.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/print/TokenFilter.java
@@ -2,6 +2,8 @@ package io.github.douira.glsl_transformer.print;
 
 import org.antlr.v4.runtime.Token;
 
+import io.github.douira.glsl_transformer.GLSLLexer;
+
 /**
  * A token filter is an object that can check if given tokens should be printed
  * or not.
@@ -11,12 +13,33 @@ public interface TokenFilter {
    * A list of the possible channels a token can have. This must correspond to the
    * generated Lexer's {@code channelNames} array.
    */
-  static enum TokenChannel {
+  public static enum TokenChannel {
     DEFAULT,
     HIDDEN,
     WHITESPACE,
     COMMENTS,
     PREPROCESSOR;
+
+    public static TokenChannel[] channels = TokenChannel.values();
+
+    // sanity check the items and their names
+    static {
+      if (channels[Token.DEFAULT_CHANNEL] != TokenChannel.DEFAULT) {
+        throw new Error("The default channel position should match ANTLR's convention!");
+      }
+      if (channels[Token.HIDDEN_CHANNEL] != TokenChannel.HIDDEN) {
+        throw new Error("The hidden channel position should match ANTLR's convention!");
+      }
+      for (var i = Token.MIN_USER_CHANNEL_VALUE; i < channels.length; i++) {
+        var enumName = channels[i].name();
+        var generatedName = GLSLLexer.channelNames[i];
+        if (!enumName.equals(generatedName)) {
+          throw new Error("The channel with name " + enumName + " at position " + i
+              + " has to match the corresponding generated channel name "
+              + generatedName + "!");
+        }
+      }
+    }
   }
 
   /**
@@ -26,7 +49,7 @@ public interface TokenFilter {
    * @return The channel of the token
    */
   static TokenChannel getTokenChannel(Token token) {
-    return TokenChannel.values()[token.getChannel()];
+    return TokenChannel.channels[token.getChannel()];
   }
 
   /**
