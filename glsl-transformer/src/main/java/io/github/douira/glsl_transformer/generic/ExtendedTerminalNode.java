284f68d61b1e146abc83a5a62403338ab120f6e8
douira <douira100@gmail.com>
2022-01-08 02:10:55 +0100

    added some javadoc

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/generic/ExtendedTerminalNode.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/generic/ExtendedTerminalNode.java
@@ -4,12 +4,33 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 
+/**
+ * The extended terminal node has additional functionality over the regular
+ * terminal node. Most importantly, it implements the functionality required to
+ * be a tree member.
+ */
 public class ExtendedTerminalNode extends TerminalNodeImpl implements TreeMember {
+  /**
+   * Creates a new extended terminal node with a parent node and a token as the
+   * symbol.
+   * 
+   * @param parent The parent node. This is a parser rule context because the
+   *               parser passes a parser rule context when it creates new
+   *               terminal nodes.
+   * @param symbol The token to contain with this terminal node
+   */
   public ExtendedTerminalNode(ParserRuleContext parent, Token symbol) {
     super(symbol);
     setParent(parent);
   }
 
+  /**
+   * Creates a null-token terminal node that has an extended context as a parent.
+   * This should only be used internally when the node is a placeholder for
+   * something. It will not be printed since the token is null.
+   * 
+   * @param parent The parent node
+   */
   protected ExtendedTerminalNode(ExtendedContext parent) {
     this(parent, null);
   }
--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/generic/TreeMember.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/generic/TreeMember.java
@@ -3,10 +3,20 @@ package io.github.douira.glsl_transformer.generic;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 /**
- * A tree member has a parent and its tokens can be omitted.
+ * A tree member has a parent and its tokens can be omitted. This interface
+ * serves as the basis for all tree parse tree nodes.
  */
 public interface TreeMember extends ParseTree {
+  /**
+   * Gets the parent of this node. The parent is an {@link ExtendedContext}
+   * because in this tree all parents should be an extended context.
+   */
   public ExtendedContext getParent();
 
+  /**
+   * Omits the tokens this node encompasses from the next local root. For terminal
+   * nodes this is always at least the parent's local root. Other nodes may be
+   * their own local root.
+   */
   public void omitTokens();
 }
