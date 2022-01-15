01eb0858a32dd448c2497dd7e7a6430c65074096
douira <douira100@gmail.com>
2022-01-15 16:59:58 +0100

    added throw target test

---
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/core/target/ThrowTargetTest.java
@@ -0,0 +1,49 @@
+package io.github.douira.glsl_transformer.core.target;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.douira.glsl_transformer.TestWithTransformationManager;
+import io.github.douira.glsl_transformer.core.SearchTerminals;
+import io.github.douira.glsl_transformer.transform.SemanticException;
+import io.github.douira.glsl_transformer.tree.TreeMember;
+
+public class ThrowTargetTest extends TestWithTransformationManager {
+  private int nextIndex;
+
+  @Test
+  void testFromMessage() {
+    var target = ThrowTarget.fromMessage("needle", "message");
+    assertEquals("needle", target.getNeedle());
+    assertEquals("message", target.getMessage(null, null));
+  }
+
+  @Test
+  void testThrow() {
+    var exception = new SemanticException("message");
+    nextIndex = 0;
+    try {
+      runTransformation("int f = foo + oofevilinside;", new SearchTerminals(new ThrowTarget("evil") {
+        @Override
+        public SemanticException getMessage(TreeMember node, String match) {
+          nextIndex++;
+          return exception;
+        }
+      }));
+    } catch (SemanticException e) {
+      assertEquals(exception, e, "It should throw the right exception");
+      nextIndex += 100;
+    }
+    assertEquals(101, nextIndex,
+        "It should visit the right points in the control flow");
+  }
+
+  @Test
+  void testNoThrow() {
+    assertDoesNotThrow(
+        () -> runTransformation(
+            "int f = foo + EvilInCaps + EVILCAPS + e_v_i_l_spaced;",
+            new SearchTerminals(ThrowTarget.fromMessage("evil", "message"))));
+  }
+}
