9aae8193181b582791dcb4343adb7a9426549482
douira <douira100@gmail.com>
2022-01-16 03:19:47 +0100

    actually apply the filter to printing

---
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/FilterTokenSource.java
@@ -0,0 +1,52 @@
+package io.github.douira.glsl_transformer.transform;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenFactory;
+import org.antlr.v4.runtime.TokenSource;
+
+public class FilterTokenSource implements TokenSource {
+
+  @Override
+  public int getCharPositionInLine() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public CharStream getInputStream() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int getLine() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public String getSourceName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public TokenFactory<?> getTokenFactory() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Token nextToken() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void setTokenFactory(TokenFactory<?> factory) {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
