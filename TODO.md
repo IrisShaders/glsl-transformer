6506dd7cade583eff6a4f9d3549bae93ee4b9002
douira <douira100@gmail.com>
2022-01-16 04:14:58 +0100

    added pre-parse token filtering

--- TODO.md
+++ TODO.md
@@ -1,9 +1,7 @@
 # Todo
 
-- Token filtering also before parsing
 - Add transformation that does wrapping: check for presence of wrap replacement, inject wrapper code, perform identifier replacement with expression or terminal
 - Some sort of additional parameter system for passing per-job state to transformation phases is required. (in order to have them activate/deactivate using the already present isActive system)
-- Reduce the amount of `instanceof` usage, especially around RunPhase/WalkPhase
 - Test ReplaceTerminals static methods
 - Test addConcurrentPhase on Transformation
 - Test the token filters and all their associated functionality (on transformation manager, printer and all the token filters themselves)
@@ -21,7 +19,3 @@
 - Macro expansion (preprocessor) and other functions?
 
 - DynamicParseTreeWalker: have transformation phase tell it about movements in the child array it caused to avoid inserting placeholder nodes (and then also avoid even doing the compacting step if it's not necessary)
-
-## Other
-
-- Publish Java 8-compatible version as `glsl-transformer-j8` instead of it being the main version. This would only be useful in case testing doesn't find a bug that jabel introduced into the J8 version that version being tested on J16/17 doesn't have. The main version would be "safe" from this kind of bug in jabel since it's the same exact code as is being tested.
--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/FilterTokenSource.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/FilterTokenSource.java
@@ -5,48 +5,102 @@ import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.TokenFactory;
 import org.antlr.v4.runtime.TokenSource;
 
-public class FilterTokenSource implements TokenSource {
+import io.github.douira.glsl_transformer.print.filter.TokenFilter;
 
-  @Override
-  public int getCharPositionInLine() {
-    // TODO Auto-generated method stub
-    return 0;
+/**
+ * The filter token source wraps another token source but reads tokens from it
+ * until a given filter accepts one. When it encounters EOF it immediately
+ * returns it.
+ */
+public class FilterTokenSource implements TokenSource {
+  private TokenSource source;
+  private TokenFilter filter;
+
+  /**
+   * Creates a new filtering token source with a given token source to wrap.
+   * 
+   * @param tokenSource The real token source to get tokens from
+   */
+  public FilterTokenSource(TokenSource tokenSource) {
+    this.source = tokenSource;
   }
 
+  /**
+   * Sets the token source on this filtering token source wrapper. May not be
+   * {@code null}.
+   * 
+   * @param tokenSource The new token source
+   */
+  public void setTokenSource(TokenSource tokenSource) {
+    this.source = tokenSource;
+  }
+
+  /**
+   * Sets the token filter on this filtering token source wrapper. Set to
+   * {@code null} to effectively disable any manipulation of the tokens generated
+   * by the contained token source.
+   * 
+   * @param tokenFilter The new token filter
+   */
+  public void setTokenFilter(TokenFilter tokenFilter) {
+    this.filter = tokenFilter;
+  }
+
+  /**
+   * Uses {@link org.antlr.v4.runtime.TokenSource#nextToken()} on the real token
+   * source to filter to get tokens and then filters them with the contained token
+   * filter if there is one.
+   * 
+   * {@inheritDoc}
+   */
   @Override
-  public CharStream getInputStream() {
-    // TODO Auto-generated method stub
-    return null;
+  public Token nextToken() {
+    if (source == null) {
+      throw new IllegalStateException("Missing a token source but a token was requested!");
+    }
+
+    if (filter == null) {
+      return source.nextToken();
+    }
+
+    while (true) {
+      var token = source.nextToken();
+      if (token.getType() == Token.EOF) {
+        return token;
+      }
+      if (filter.isTokenAllowed(token)) {
+        return token;
+      }
+    }
   }
 
   @Override
   public int getLine() {
-    // TODO Auto-generated method stub
-    return 0;
+    return source.getLine();
+  }
+
+  @Override
+  public int getCharPositionInLine() {
+    return source.getCharPositionInLine();
+  }
+
+  @Override
+  public CharStream getInputStream() {
+    return source.getInputStream();
   }
 
   @Override
   public String getSourceName() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public TokenFactory<?> getTokenFactory() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public Token nextToken() {
-    // TODO Auto-generated method stub
-    return null;
+    return source.getSourceName();
   }
 
   @Override
   public void setTokenFactory(TokenFactory<?> factory) {
-    // TODO Auto-generated method stub
-    
+    source.setTokenFactory(factory);
   }
-  
+
+  @Override
+  public TokenFactory<?> getTokenFactory() {
+    return source.getTokenFactory();
+  }
 }
