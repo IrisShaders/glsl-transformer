e3e4b156e1f843c74d18c267cfd187e2230f8211
douira <douira100@gmail.com>
2022-02-04 12:15:18 +0100

    finished multi filter tests

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/print/filter/MultiFilterTest.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/print/filter/MultiFilterTest.java
@@ -2,6 +2,7 @@ package io.github.douira.glsl_transformer.print.filter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.antlr.v4.runtime.Token;
 import org.junit.jupiter.api.Test;
 
 import io.github.douira.glsl_transformer.TestWithTransformationManager;
@@ -9,12 +10,19 @@ import io.github.douira.glsl_transformer.transform.TransformationManager;
 import io.github.douira.glsl_transformer.util.CompatUtil;
 
 public class MultiFilterTest extends TestWithTransformationManager<Void> {
+  int nextIndex;
+
   static <T> TransformationManager<T> assertPrintFilterResult(
-      String expected, String input, TokenFilter<T> filter, String message) {
+      String expected, String input, T parameters, TokenFilter<T> filter, String message) {
     var man = new TransformationManager<T>();
     man.setPrintTokenFilter(filter);
-    assertEquals(expected, man.transform(input), message);
+    assertEquals(expected, man.transform(input, parameters), message);
     return man;
+  }
+
+  static <T> TransformationManager<T> assertPrintFilterResult(
+      String expected, String input, TokenFilter<T> filter, String message) {
+    return assertPrintFilterResult(expected, input, null, filter, message);
   }
 
   @Test
@@ -59,22 +67,105 @@ public class MultiFilterTest extends TestWithTransformationManager<Void> {
 
   @Test
   void testResetState() {
-    // test that the state is reset on the subfilters
+    nextIndex = 0;
+    var multiFilter = new MultiFilter<Void>();
+    multiFilter.add(new StringFilter<Void>("a") {
+      boolean finished;
+
+      @Override
+      public boolean isTokenAllowed(Token token) {
+        if (finished) {
+          return true;
+        }
+
+        if (super.isTokenAllowed(token)) {
+          finished = true;
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      @Override
+      public void resetState() {
+        finished = false;
+        nextIndex++;
+      }
+    });
+    multiFilter.add(new StringFilter<Void>("b") {
+      @Override
+      public void resetState() {
+        nextIndex += 100;
+      }
+    });
+
+    assertPrintFilterResult(
+        ";;;a;c;", "a;b;b;a;c;", multiFilter, "It should filter correctly");
+    assertEquals(101, nextIndex, "It should reset the state of the subfilters");
+    assertPrintFilterResult(
+        ";;;a;c;", "a;b;b;a;c;", multiFilter, "It should filter correctly again");
   }
 
   @Test
   void testSetCollector() {
-    // test that the collector is set on the subfilters and that all of them can
-    // access job parameters
+    nextIndex = 0;
+    var parameters = new Object();
+    var multiFilter = new MultiFilter<Object>();
+    multiFilter.add(new StringFilter<Object>("a") {
+      @Override
+      public boolean isTokenAllowed(Token token) {
+        nextIndex++;
+        assertEquals(parameters, getJobParameters(), "It should have job parameters during filtering");
+        return super.isTokenAllowed(token);
+      }
+
+      @Override
+      public void resetState() {
+        nextIndex += 100;
+        assertEquals(parameters, getJobParameters(), "It should have job parameters during state reset");
+      }
+    });
+
+    assertPrintFilterResult(";b;", "a;b;", parameters, multiFilter, "It should filter correctly");
+    assertEquals(104, nextIndex, "It should run reset and the filtering method");
   }
 
   @Test
-  void testSetConjunction() {
-    // test AND/OR behavior
-  }
+  void testConjunctionAndShortCircuit() {
+    nextIndex = 0;
+    var multiFilter = new MultiFilter<Void>();
+    multiFilter.add(new TokenFilter<Void>() {
+      @Override
+      public boolean isTokenAllowed(Token token) {
+        nextIndex++;
+        return !token.getText().toLowerCase().equals("a");
+      }
+    });
+    multiFilter.add(new TokenFilter<Void>() {
+      @Override
+      public boolean isTokenAllowed(Token token) {
+        nextIndex += 100;
+        return !token.getText().matches("[A-Z]");
+      }
+    });
 
-  @Test
-  void testSetShortCircuit() {
-    // test shortcircuiting enabled/disabled
+    nextIndex = 0;
+    assertPrintFilterResult(";b;;;", "a;b;A;B;", multiFilter, "It should filter with a conjunction by default");
+    assertEquals(808, nextIndex, "It should call both filters each time");
+    
+    nextIndex = 0;
+    multiFilter.setConjunction(false);
+    assertPrintFilterResult("a;b;;B;", "a;b;A;B;", multiFilter, "It should filter correctly with a disjunction");
+    assertEquals(808, nextIndex, "It should call both filters each time");
+
+    nextIndex = 0;
+    multiFilter.setShortCircuit(true);
+    assertPrintFilterResult("a;b;;B;", "a;b;A;B;", multiFilter, "It should filter correctly with a disjunction");
+    assertEquals(208, nextIndex, "It should call call the second filter only when the first one is false");
+
+    nextIndex = 0;
+    multiFilter.setConjunction(true);
+    assertPrintFilterResult(";b;;;", "a;b;A;B;", multiFilter, "It should filter correctly with a conjunction");
+    assertEquals(608, nextIndex, "It should call call the second filter only when the first one is true");
   }
 }
