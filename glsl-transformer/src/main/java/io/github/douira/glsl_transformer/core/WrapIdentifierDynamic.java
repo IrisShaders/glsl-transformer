a98fab5bee6069125214e5c7b30f2f50ea9ed838
douira <douira100@gmail.com>
2022-02-05 04:04:27 +0100

    added javadoc

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/core/WrapIdentifierDynamic.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/core/WrapIdentifierDynamic.java
@@ -5,12 +5,29 @@ import io.github.douira.glsl_transformer.core.target.WrapThrowTarget;
 import io.github.douira.glsl_transformer.transform.Transformation;
 import io.github.douira.glsl_transformer.transform.TransformationPhase;
 
+/**
+ * The dynamic wrap identifier transformation has a number of abstract methods
+ * that can be implemented in order to dynamically supply the parameters for
+ * this identifier wrap operation. The wrapping injector is given directly
+ * though since the injection methods can vary greatly.
+ */
 public abstract class WrapIdentifierDynamic<T> extends Transformation<T> {
+  /**
+   * Creates a new wrap identifier transformation with a specified wrapping
+   * injector phase.
+   * 
+   * @param wrappingInjector A phase that injects the additional code into the
+   *                         tree required for making the wrap work
+   */
   public WrapIdentifierDynamic(TransformationPhase<T> wrappingInjector) {
     this();
     addConcurrentPhase(wrappingInjector);
   }
 
+  /**
+   * Creates a new wrap identifier transformation with no wrapping injector. The
+   * phase for that should be added to the transformation manually.
+   */
   protected WrapIdentifierDynamic() {
     addPhase(new SearchTerminals<T>(new WrapThrowTarget<T>() {
       @Override
@@ -42,9 +59,24 @@ public abstract class WrapIdentifierDynamic<T> extends Transformation<T> {
     });
   }
 
+  /**
+   * Returns if the phases should be active.
+   * 
+   * @return {@code true} if the phases should be active
+   */
   protected abstract boolean isActiveDynamic();
 
+  /**
+   * Returns the wrapping result that is used as a replacement for the target.
+   * 
+   * @return The wrapping result
+   */
   protected abstract String getWrapResultDynamic();
 
+  /**
+   * Returns the wrapping target. The wrapping target is the identifier that is replaced with the wrap result.
+   * 
+   * @return The wrapping target
+   */
   protected abstract String getWrapTargetDynamic();
 }
--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/core/WrapIdentifierExternalDeclaration.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/core/WrapIdentifierExternalDeclaration.java
@@ -2,18 +2,25 @@ package io.github.douira.glsl_transformer.core;
 
 import io.github.douira.glsl_transformer.GLSLParser.TranslationUnitContext;
 import io.github.douira.glsl_transformer.transform.RunPhase;
+import io.github.douira.glsl_transformer.transform.TransformationPhase.InjectionPoint;
 
+/**
+ * A wrap identifier transformation that injects an external declaration at a
+ * specified location. By default it's injected at
+ * {@link InjectionPoint#BEFORE_DECLARATIONS}.
+ */
 public abstract class WrapIdentifierExternalDeclaration<T>
     extends WrapIdentifierDynamic<T> {
-
+  /**
+   * Creates a new wrap identifier transformation that injects an external
+   * declaration.
+   */
   public WrapIdentifierExternalDeclaration() {
     super();
     addConcurrentPhase(new RunPhase<T>() {
       @Override
       protected void run(TranslationUnitContext ctx) {
-        injectExternalDeclaration(
-            InjectionPoint.BEFORE_DECLARATIONS,
-            getInjectionContent());
+        injectExternalDeclaration(getInjectionLocation(), getInjectionContent());
       }
 
       @Override
@@ -23,5 +30,21 @@ public abstract class WrapIdentifierExternalDeclaration<T>
     });
   }
 
+  /**
+   * Returns the content that is injected as an external declaration.
+   * 
+   * @return The string to be parsed into a new external declaration
+   */
   protected abstract String getInjectionContent();
+
+  /**
+   * Returns the injection location for where the external declaration should be
+   * injected. By default this is {@link InjectionPoint#BEFORE_DECLARATIONS} but
+   * it can be overwritten.
+   * 
+   * @return The injection location to use
+   */
+  protected InjectionPoint getInjectionLocation() {
+    return InjectionPoint.BEFORE_DECLARATIONS;
+  }
 }
