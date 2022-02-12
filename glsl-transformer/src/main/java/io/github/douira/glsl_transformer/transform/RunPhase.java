3bcc6c3390d30a31c9fdba0fc3d03e5723c86975
douira <douira100@gmail.com>
2022-02-12 15:43:51 +0100

    added inject run phase methods for compactness

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/RunPhase.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/RunPhase.java
@@ -1,5 +1,7 @@
 package io.github.douira.glsl_transformer.transform;
 
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import io.github.douira.glsl_transformer.GLSLParser.TranslationUnitContext;
 
 /**
@@ -27,5 +29,45 @@ public abstract class RunPhase<T> extends TransformationPhase<T> {
 
   @Override
   final protected void runAfterWalk(TranslationUnitContext ctx) {
+  }
+
+  /**
+   * Returns a new run phase that injects the given nodes at the given location.
+   * 
+   * @see TransformationPhase#injectNodes(InjectionPoint, ParseTree...)
+   * 
+   * @param <R>      The job parameter type
+   * @param location The location to inject the nodes at
+   * @param newNodes The nodes to inject
+   * @return The run phase that does only these injections
+   */
+  public <R> RunPhase<R> withInjectNodes(InjectionPoint location, ParseTree newNodes) {
+    return new RunPhase<R>() {
+      @Override
+      protected void run(TranslationUnitContext ctx) {
+        injectNodes(location, newNodes);
+      }
+    };
+  }
+
+  /**
+   * Returns a new run phase that injects the given strings as an external
+   * declarations at the given location.
+   * 
+   * @see TransformationPhase#injectExternalDeclarations(InjectionPoint,
+   *      String...)
+   * 
+   * @param <R>      The job parameter type
+   * @param location The location to inject the external declarations at
+   * @param str      The strings to parse as external declarations
+   * @return The run phase that does only these injections
+   */
+  public <R> RunPhase<R> withInjectExternalDeclarations(InjectionPoint location, String... str) {
+    return new RunPhase<R>() {
+      @Override
+      protected void run(TranslationUnitContext ctx) {
+        injectExternalDeclarations(location, str);
+      }
+    };
   }
 }
