e375e3e6a16723c5669b555218edc5c50b30d27e
douira <douira100@gmail.com>
2022-03-03 05:05:50 +0100

    added more method variations and new methods

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/Transformation.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/Transformation.java
@@ -46,7 +46,7 @@ public class Transformation<T> extends LifecycleUserImpl<T> {
    *              transformation
    */
   public Transformation(TransformationPhase<T> phase) {
-    concurrent(phase);
+    addRootDependency(phase);
   }
 
   /**
@@ -63,7 +63,7 @@ public class Transformation<T> extends LifecycleUserImpl<T> {
     return endNode;
   }
 
-  Node<T> getNode(LifecycleUser<T> content) {
+  private Node<T> getNode(LifecycleUser<T> content) {
     return Optional.ofNullable(nodes.get(content)).orElseGet(() -> {
       var newNode = new Node<T>(content);
       nodes.put(content, newNode);
@@ -84,29 +84,112 @@ public class Transformation<T> extends LifecycleUserImpl<T> {
     dependencyNode.updateBothLinks(rootNode, endNode);
   }
 
+  /**
+   * Adds a dependency between two nodes. This means the dependency will be run
+   * before the dependent. Both of them are added to this transformation if not
+   * already present.
+   * 
+   * @param dependent  The node depending on the dependency to have been run first
+   * @param dependency The node that needs to be run before the dependent
+   */
   public void addDependency(LifecycleUser<T> dependent, LifecycleUser<T> dependency) {
     addDependency(getNode(dependent), getNode(dependency));
   }
 
-  public void chainDependency(LifecycleUser<T> dependency) {
-    addDependency(lastAddedDependency, getNode(dependency));
+  public void addDependent(LifecycleUser<T> dependent, LifecycleUser<T> dependency) {
+    addDependency(dependency, dependent);
   }
 
-  public void append(LifecycleUser<T> toAppend) {
+  /**
+   * Adds a dependency for the last added dependency. If this is the first
+   * dependency added to this transformation, this adds it as a dependency of the
+   * root node.
+   * 
+   * @param dependency The node to add as a further dependency
+   */
+  public LifecycleUser<T> chainDependency(LifecycleUser<T> dependency) {
+    addDependency(Optional.ofNullable(lastAddedDependency).orElse(rootNode), getNode(dependency));
+    return dependency;
+  }
+
+  public LifecycleUser<T> chainDependent(LifecycleUser<T> dependent) {
+    addDependency(getNode(dependent), Optional.ofNullable(lastAddedDependency).orElse(endNode));
+    return dependent;
+  }
+
+  /**
+   * Adds a dependency between the end node and all of its dependents. This
+   * replaces the end node with a new end node.
+   * 
+   * @param toAppend
+   */
+  public LifecycleUser<T> append(LifecycleUser<T> toAppend) {
     var previousEnd = endNode;
     endNode = new Node<T>();
     previousEnd.setContent(toAppend);
+    nodes.put(toAppend, previousEnd);
     previousEnd.addDependency(endNode);
+    return toAppend;
   }
 
-  public void prepend(LifecycleUser<T> toPrepend) {
+  /**
+   * Adds a dependency between the root node and all of its dependencies. This
+   * replaces the root node with a new root node.
+   * 
+   * @param toPrepend The node to place before all present dependencies
+   */
+  public LifecycleUser<T> prepend(LifecycleUser<T> toPrepend) {
     var previousRoot = rootNode;
     rootNode = new Node<T>();
     previousRoot.setContent(toPrepend);
+    nodes.put(toPrepend, previousRoot);
     rootNode.addDependency(previousRoot);
+    return toPrepend;
   }
 
-  public Node<T> concurrent(LifecycleUser<T> dependency) {
-    return rootNode.addDependency(getNode(dependency));
+  /**
+   * Adds a dependency for the root node. All dependencies added by this method
+   * can be run concurrently.
+   * 
+   * @param dependency The node to add as a root dependency
+   * @return The added node
+   */
+  public LifecycleUser<T> addRootDependency(LifecycleUser<T> dependency) {
+    var dependencyNode = getNode(dependency);
+    addDependency(rootNode, dependencyNode);
+    return dependency;
   }
+
+  public LifecycleUser<T> addEndDependent(LifecycleUser<T> dependent) {
+    var dependentNode = getNode(dependent);
+    addDependency(dependentNode, endNode);
+    return dependent;
+  }
+
+  /**
+   * Adds a dependency for the dependent of the last added dependency. If there is
+   * no such dependency this method will throw. The dependencies added by this
+   * method can be run concurrently with other methods added by it.
+   * 
+   * @param dependency The node to add as a dependency of the last added
+   *                   dependency's dependent
+   * @return The added node
+   */
+  public LifecycleUser<T> chainConcurrentDependency(LifecycleUser<T> dependency) {
+    if (lastAddedDependency == null) {
+      throw new IllegalStateException(
+          "Can't add a concurrent dependency for the last added dependency if none was added yet!");
+    }
+    var latestDependent = lastAddedDependency.getLatestDependent();
+    if (latestDependent == null) {
+      throw new IllegalStateException(
+          "Can't add a concurrent dependency for the last added dependency if it was removed again! Only the last dependency is stored for this feature.");
+    }
+
+    var dependencyNode = getNode(dependency);
+    addDependency(latestDependent, dependencyNode);
+    return dependency;
+  }
+
+  //TODO: chainConcurrentDependent
 }
