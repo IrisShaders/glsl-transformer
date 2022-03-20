2a4ac66c590d5f05109ac85e11fb8750d5798fb8
douira <douira100@gmail.com>
2022-03-20 22:46:36 +0100

    added deep expression test file

---
+++ glsl-transformer/src/test/resources/deepExpressionTest.glsl
@@ -0,0 +1,5 @@
+mat4 m34 = mat4(v.x*u.x, v.x*u.y, v.x*u.z, v.x*u.w,
+                    v.y*u.x, v.y*u.y, v.y*u.z, v.y*u.w,
+                    v.z*u.x, v.z*u.y, v.z*u.z, v.z*u.w,
+                    v.w*u.x, v.w*u.y, v.w*u.z, v.w*u.w);
+// mat4 m34 = mat4(v.x*u.x, v.x*u.y);
