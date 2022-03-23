ce85f60050afa958582963b4750069373893c5b2
douira <douira100@gmail.com>
2022-03-23 03:03:01 +0100

    enabled phases to control how much the parse tree walker has to work

---
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/PartialParseTreeListener.java
@@ -0,0 +1,37 @@
+package io.github.douira.glsl_transformer.transform;
+
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+import io.github.douira.glsl_transformer.tree.ExtendedContext;
+
+/**
+ * Adds a method to the parse tree listener with which the parse tree walker can
+ * detect if it should stop walking the tree.
+ */
+public interface PartialParseTreeListener extends ParseTreeListener {
+  /**
+   * Returns if this parse tree listener is no longer interested in walking the
+   * tree.
+   * 
+   * @apiNote This method will be called at every level of the tree as the parse
+   *          tree walker decides whether or not to continue iterating the
+   *          children of each node.
+   * 
+   * @return {@code true} if the parse tree walker should stop walking the tree
+   *         entirely.
+   */
+  default boolean isFinished() {
+    return false;
+  }
+
+  /**
+   * Returns if this parse tree listener wants to continue to walk the tree but
+   * not go any deeper into the current (given) rule.
+   * 
+   * @param node The current node in which to maybe not visit the children
+   * @return {@code true} if the parse tree walker
+   */
+  default boolean isDeepEnough(ExtendedContext node) {
+    return false;
+  }
+}
--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/ProxyParseTreeListener.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/ProxyParseTreeListener.java
@@ -1,33 +1,37 @@
 package io.github.douira.glsl_transformer.transform;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
-import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.TerminalNode;
+
+import io.github.douira.glsl_transformer.tree.ExtendedContext;
 
 /**
  * This class was taken from <a href=
  * "https://github.com/antlr/antlr4/issues/841#issuecomment-84450146">here</a>
  * and was added to the public domain by the author DaveJarvis.
  * 
- * Instances of this class allows multiple listeners to receive events while
+ * Instances of this class allow multiple listeners to receive events while
  * walking the parse tree. For example:
  *
  * <pre>
  * ProxyParseTreeListener proxy = new ProxyParseTreeListener();
  * ParseTreeListener listener1 = ... ;
  * ParseTreeListener listener2 = ... ;
- * proxy.add( listener1 );
- * proxy.add( listener2 );
- * ParseTreeWalker.DEFAULT.walk( proxy, ctx );
+ * proxy.add(listener1);
+ * proxy.add(listener2);
+ * ParseTreeWalker.DEFAULT.walk(proxy, ctx);
  * </pre>
  */
-public class ProxyParseTreeListener implements ParseTreeListener {
-  private List<ParseTreeListener> listeners;
+public class ProxyParseTreeListener implements PartialParseTreeListener {
+  private Collection<PartialParseTreeListener> listeners;
+  private Iterator<PartialParseTreeListener> listenerIterator;
 
   /**
    * Creates a new proxy without an empty list of listeners. Add listeners before
@@ -40,87 +44,127 @@ public class ProxyParseTreeListener implements ParseTreeListener {
 
   /**
    * Creates a new proxy with the given list of listeners.
-   *
-   * @param listeners A list of listerners to receive events.
+   * 
+   * @param listeners A list of listeners to receive events.
    */
-  public ProxyParseTreeListener(List<ParseTreeListener> listeners) {
+  public ProxyParseTreeListener(Collection<PartialParseTreeListener> listeners) {
     setListeners(listeners);
-  }
-
-  @Override
-  public void enterEveryRule(ParserRuleContext ctx) {
-    for (ParseTreeListener listener : getListeners()) {
-      listener.enterEveryRule(ctx);
-      ctx.enterRule(listener);
-    }
-  }
-
-  @Override
-  public void exitEveryRule(ParserRuleContext ctx) {
-    for (ParseTreeListener listener : getListeners()) {
-      ctx.exitRule(listener);
-      listener.exitEveryRule(ctx);
-    }
-  }
-
-  @Override
-  public void visitErrorNode(ErrorNode node) {
-    for (ParseTreeListener listener : getListeners()) {
-      listener.visitErrorNode(node);
-    }
-  }
-
-  @Override
-  public void visitTerminal(TerminalNode node) {
-    for (ParseTreeListener listener : getListeners()) {
-      listener.visitTerminal(node);
-    }
   }
 
   /**
    * Adds the given listener to the list of event notification recipients.
-   *
+   * 
    * @param listener A listener to begin receiving events.
    */
-  public void add(ParseTreeListener listener) {
+  public void add(PartialParseTreeListener listener) {
     getListeners().add(listener);
   }
 
   /**
    * Removes the given listener to the list of event notification recipients.
-   *
+   * 
    * @param listener A listener to stop receiving events.
    * @return false The listener was not registered to receive events.
    */
-  public boolean remove(ParseTreeListener listener) {
+  public boolean remove(PartialParseTreeListener listener) {
     return getListeners().remove(listener);
   }
 
   /**
    * Returns the list of listeners.
-   *
+   * 
    * @return The list of listeners to receive tree walking events.
    */
-  private List<ParseTreeListener> getListeners() {
+  private Collection<PartialParseTreeListener> getListeners() {
     return this.listeners;
   }
 
   /**
    * Changes the list of listeners to receive events. If the given list of
    * listeners is null, an empty list will be created.
-   *
+   * 
    * @param listeners A list of listeners to receive tree walking events.
    */
-  public void setListeners(List<ParseTreeListener> listeners) {
-    this.listeners = Optional.ofNullable(listeners).orElseGet(ArrayList<ParseTreeListener>::new);
+  public void setListeners(Collection<PartialParseTreeListener> listeners) {
+    this.listeners = Optional.ofNullable(listeners).orElseGet(ArrayList<PartialParseTreeListener>::new);
   }
 
   /**
    * Checks if the list of listeners is empty.
    * 
-   * @return {@code true} if there are not listeners in the list
+   * @return {@code true} if the list of listeners is empty
    */
   public boolean isEmpty() {
     return this.listeners.isEmpty();
+  }
+
+  /**
+   * Removes the listener last processed during iteration from the list of
+   * listeners. This is used by the execution planner to remove nodes that are
+   * finished with walking.
+   */
+  public void removeCurrentListener() {
+    listenerIterator.remove();
+  }
+
+  private void iterateListeners(Consumer<PartialParseTreeListener> consumer) {
+    listenerIterator = getListeners().iterator();
+    while (listenerIterator.hasNext()) {
+      consumer.accept(listenerIterator.next());
+    }
+    listenerIterator = null;
+  }
+
+  @Override
+  public boolean isDeepEnough(ExtendedContext node) {
+    for (var listener : getListeners()) {
+      if (!listener.isDeepEnough(node)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public boolean isFinished() {
+    if (isEmpty()) {
+      return true;
+    }
+    for (var listener : getListeners()) {
+      if (!listener.isFinished()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public void enterEveryRule(ParserRuleContext ctx) {
+    iterateListeners(listener -> {
+      listener.enterEveryRule(ctx);
+      ctx.enterRule(listener);
+    });
+  }
+
+  @Override
+  public void exitEveryRule(ParserRuleContext ctx) {
+    iterateListeners(listener -> {
+      ctx.exitRule(listener);
+      listener.exitEveryRule(ctx);
+    });
+  }
+
+  @Override
+  public void visitErrorNode(ErrorNode node) {
+    iterateListeners(listener -> {
+      listener.visitErrorNode(node);
+    });
+  }
+
+  @Override
+  public void visitTerminal(TerminalNode node) {
+    iterateListeners(listener -> {
+      listener.visitTerminal(node);
+    });
   }
 }
