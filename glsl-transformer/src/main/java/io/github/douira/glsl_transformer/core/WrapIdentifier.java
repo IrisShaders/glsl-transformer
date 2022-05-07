d0b3766e7a16ea2ddd67fe919c49d76ddc8ae03e
douira <douira100@gmail.com>
2022-05-07 04:18:35 +0200

    added javadoc

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/core/WrapIdentifier.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/core/WrapIdentifier.java
@@ -14,18 +14,23 @@ import io.github.douira.glsl_transformer.tree.ExtendedContext;
  * with new code by replacing its usage with a new expression and inserting code
  * that takes care of handling the conversion from the new to the old value. It
  * also checks that the wrapped value isn't already present in the code.
+ * 
+ * This class uses a chaining configuration pattern. The class is constructed
+ * with an empty constructor and then configured by calling some configuration
+ * methods. Additionally, configuration values can be generated dynamically by
+ * overriding the getter methods.
  */
 public class WrapIdentifier<T extends JobParameters> extends Transformation<T> {
   private ActivatableLifecycleUser<T> wrapResultDetector;
-  private String wrapResult;
-  private String wrapExpression;
+  private String detectionResult;
+  private String parsedReplacement;
   private Function<GLSLParser, ExtendedContext> parseMethod;
 
   private ActivatableLifecycleUser<T> wrappingReplacer;
   private HandlerTarget<T> wrapHandlerTarget;
   private String wrapTarget;
 
-  private ActivatableLifecycleUser<T> wrappingInjector;
+  private ActivatableLifecycleUser<T> injector;
   private InjectionPoint injectionLocation;
   private String injectionExternalDeclaration;
 
@@ -43,107 +48,228 @@ public class WrapIdentifier<T extends JobParameters> extends Transformation<T> {
   protected void setupGraph() {
     chainDependent(getWrapResultDetector());
     chainDependent(getWrappingReplacer());
-    chainDependent(getWrappingInjector());
+    chainDependent(getInjector());
   }
 
+  /**
+   * Sets the detector that will be used to check if the detection result exists
+   * already. Generated from the detection result by default.
+   * 
+   * @param wrapResultDetector The result detector
+   * @return This object
+   */
   public WrapIdentifier<T> wrapResultDetector(ActivatableLifecycleUser<T> wrapResultDetector) {
     this.wrapResultDetector = wrapResultDetector;
     return this;
   }
 
-  public WrapIdentifier<T> wrapResult(String wrapResult) {
-    this.wrapResult = wrapResult;
+  /**
+   * Sets the result that will be present in the code after the wrapping.
+   * Generated from the parsed replacement by default. However, if a parsed
+   * replacement is used, the detection result should usually be set manually if
+   * the replacement isn't just a single identifier. If used without an explicit
+   * expression to insert, this must be an identifier since it's not parsed.
+   * 
+   * @param detectionResult The detection result
+   * @return This object
+   */
+  public WrapIdentifier<T> detectionResult(String detectionResult) {
+    this.detectionResult = detectionResult;
     return this;
   }
 
-  public WrapIdentifier<T> wrapExpression(String wrapExpression) {
-    this.wrapExpression = wrapExpression;
+  /**
+   * Sets the parsed replacement that replaces the target. Has no default but
+   * is only used if a parser method is set.
+   * 
+   * @param parsedReplacement The parsed replacement
+   * @return This object
+   */
+  public WrapIdentifier<T> parsedReplacement(String parsedReplacement) {
+    this.parsedReplacement = parsedReplacement;
     return this;
   }
 
+  /**
+   * Sets the parser method that will be used to parse the wrap expression. Is the
+   * default expression parsing method by default.
+   * 
+   * @param parseMethod The replacement parsing method
+   * @return This object
+   */
   public WrapIdentifier<T> parseMethod(Function<GLSLParser, ExtendedContext> parseMethod) {
     this.parseMethod = parseMethod;
     return this;
   }
 
+  /**
+   * Sets the replacer that will be used to replace the target with the wrap
+   * expression.
+   * 
+   * @param wrappingReplacer The replacer
+   * @return This object
+   */
   public WrapIdentifier<T> wrappingReplacer(ActivatableLifecycleUser<T> wrappingReplacer) {
     this.wrappingReplacer = wrappingReplacer;
     return this;
   }
 
+  /**
+   * Sets the handler target that does the replacement of the target. Generated
+   * from the wrap target by default.
+   * 
+   * @param wrapHandlerTarget The replacement target handler
+   * @return This object
+   */
   public WrapIdentifier<T> wrapHandlerTarget(HandlerTarget<T> wrapHandlerTarget) {
     this.wrapHandlerTarget = wrapHandlerTarget;
     return this;
   }
 
+  /**
+   * Sets the target that will be replaced.
+   * 
+   * @param wrapTarget The replacement target identifier
+   * @return This object
+   */
   public WrapIdentifier<T> wrapTarget(String wrapTarget) {
     this.wrapTarget = wrapTarget;
     return this;
   }
 
-  public WrapIdentifier<T> wrappingInjector(ActivatableLifecycleUser<T> wrappingInjector) {
-    this.wrappingInjector = wrappingInjector;
+  /**
+   * Sets the injector that will be used to inject additional code after
+   * replacement is done. Generated from the injection location and the injection
+   * external declaration by default.
+   * 
+   * @param injector The injector
+   * @return This object
+   */
+  public WrapIdentifier<T> injector(ActivatableLifecycleUser<T> injector) {
+    this.injector = injector;
     return this;
   }
 
+  /**
+   * Sets the injection location for the generated injector.
+   * 
+   * @param injectionLocation The injection location
+   * @return This object
+   */
   public WrapIdentifier<T> injectionLocation(InjectionPoint injectionLocation) {
     this.injectionLocation = injectionLocation;
     return this;
   }
 
+  /**
+   * Sets the external declaration for the generated injector.
+   * 
+   * @param injectionExternalDeclaration The external declaration
+   * @return This object
+   */
   public WrapIdentifier<T> injectionExternalDeclaration(String injectionExternalDeclaration) {
     this.injectionExternalDeclaration = injectionExternalDeclaration;
     return this;
   }
 
+  /**
+   * Gets the result detector.
+   * 
+   * @return The result detector
+   */
   protected ActivatableLifecycleUser<T> getWrapResultDetector() {
     return withDefault(wrapResultDetector,
-        () -> new SearchTerminalsImpl<>(new WrapThrowTargetImpl<T>(getWrapResult())))
+        () -> new SearchTerminalsImpl<>(new WrapThrowTargetImpl<T>(detectionResult())))
         .activation(this::isActive);
   }
 
-  protected String getWrapResult() {
-    return withDefault(wrapResult, this::getWrapExpression);
+  /**
+   * Gets the detection result.
+   * 
+   * @return The detection result
+   */
+  protected String detectionResult() {
+    return withDefault(detectionResult, this::getParsedReplacement);
   }
 
-  protected String getWrapExpression() {
-    return wrapExpression;
+  /**
+   * Gets the parsed replacement.
+   * 
+   * @return The parsed replacement
+   */
+  protected String getParsedReplacement() {
+    return parsedReplacement;
   }
 
+  /**
+   * Gets the replacement expression parsing method.
+   * 
+   * @return The replacement expression parsing method
+   */
   protected Function<GLSLParser, ExtendedContext> getParseMethod() {
     return withDefault(parseMethod, GLSLParser::expression);
   }
 
+  /**
+   * Gets the wrapping replacer.
+   * 
+   * @return The wrapping replacer
+   */
   protected ActivatableLifecycleUser<T> getWrappingReplacer() {
     return withDefault(wrappingReplacer,
         () -> new SearchTerminalsImpl<T>(getWrapHandlerTarget()))
         .activation(this::isActive);
   }
 
+  /**
+   * Gets the replacement target handler.
+   * 
+   * @return The replacement target handler
+   */
   protected HandlerTarget<T> getWrapHandlerTarget() {
     return withDefault(wrapHandlerTarget, () -> {
-      var expression = getWrapExpression();
-      return expression == null
-          ? new TerminalReplaceTargetImpl<T>(getWrapTarget(), getWrapResult())
-          : new ParsedReplaceTargetImpl<T>(getWrapTarget(), expression, getParseMethod());
+      var replacement = getParsedReplacement();
+      return replacement == null
+          ? new TerminalReplaceTargetImpl<T>(getWrapTarget(), detectionResult())
+          : new ParsedReplaceTargetImpl<T>(getWrapTarget(), replacement, getParseMethod());
     });
   }
 
+  /**
+   * Gets the wrap target.
+   * 
+   * @return The wrap target
+   */
   protected String getWrapTarget() {
     return wrapTarget;
   }
 
-  protected ActivatableLifecycleUser<T> getWrappingInjector() {
-    return withDefault(wrappingInjector,
+  /**
+   * Gets the wrapping injector.
+   * 
+   * @return The injector
+   */
+  protected ActivatableLifecycleUser<T> getInjector() {
+    return withDefault(injector,
         () -> RunPhase.<T>withInjectExternalDeclarations(
             getInjectionLocation(), getInjectionExternalDeclaration()))
         .activation(this::isActive);
   }
 
+  /**
+   * Gets the injection location.
+   * 
+   * @return The injection location
+   */
   protected InjectionPoint getInjectionLocation() {
     return withDefault(injectionLocation, InjectionPoint.BEFORE_DECLARATIONS);
   }
 
+  /**
+   * Gets the injection external declaration.
+   * 
+   * @return The injection external declaration
+   */
   protected String getInjectionExternalDeclaration() {
     return injectionExternalDeclaration;
   }
--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/util/ConfigUtil.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/util/ConfigUtil.java
@@ -2,15 +2,51 @@ package io.github.douira.glsl_transformer.util;
 
 import java.util.function.Supplier;
 
+/**
+ * Holds utility methods for configuring core transformations and transformation
+ * phases.
+ */
 public class ConfigUtil {
+  /**
+   * Uses a default generator to return a value if the given set value is null. If
+   * the generated value is also null, it throws an error. This is used for
+   * writing getter methods in core classes that use the chaining configuration
+   * pattern.
+   * 
+   * @param <V>              The value type
+   * @param setValue         The value set for this configuration by the user
+   * @param defaultGenerator The default value generator
+   * @return The set value or the generated value
+   */
   public static <V> V withDefault(V setValue, Supplier<V> defaultGenerator) {
     return setValue == null ? withDefault(setValue, defaultGenerator.get()) : setValue;
   }
 
+  /**
+   * Uses a default value if the given value generator is null. If the default
+   * value is also null, an error is thrown. This is used for writing getter
+   * methods in core classes that use the chaining configuration pattern. Note
+   * that nothing special happens if the set value generator returns null.
+   * 
+   * @param <V>              The value type
+   * @param setValueSupplier The value generator set for this configuration by the
+   *                         user
+   * @param defaultValue     The default value
+   * @return The generated value or the default value
+   */
   public static <V> V withDefault(Supplier<V> setValueSupplier, V defaultValue) {
     return setValueSupplier == null ? withDefault((V) null, defaultValue) : setValueSupplier.get();
   }
 
+  /**
+   * Returns the set value if it is not null and returns the default value
+   * otherwise. If the default is also null it throws an error.
+   * 
+   * @param <V>          The value type
+   * @param setValue     The value set for this configuration by the user
+   * @param defaultValue The default value
+   * @return The set value or the default value
+   */
   public static <V> V withDefault(V setValue, V defaultValue) {
     if (setValue == null) {
       if (defaultValue == null) {
