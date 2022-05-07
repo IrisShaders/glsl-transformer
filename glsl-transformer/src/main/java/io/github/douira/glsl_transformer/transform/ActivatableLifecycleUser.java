316a5e63484a801e806043d97bc5ac6e0c99e2f8
douira <douira100@gmail.com>
2022-05-07 04:18:35 +0200

    added javadoc

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/ActivatableLifecycleUser.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/transform/ActivatableLifecycleUser.java
@@ -2,6 +2,9 @@ package io.github.douira.glsl_transformer.transform;
 
 import java.util.function.Supplier;
 
+/**
+ * Combines activatable and lifecycle user functionality.
+ */
 public interface ActivatableLifecycleUser<T extends JobParameters> extends LifecycleUser<T>, Activatable {
   /** Override to make type more specific */
   @Override
