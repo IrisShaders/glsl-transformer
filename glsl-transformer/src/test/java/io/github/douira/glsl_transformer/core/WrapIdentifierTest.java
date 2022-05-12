02ac4c81f3ab6848931c9fcf241c62725e0a99a1
douira <douira100@gmail.com>
2022-05-12 03:35:17 +0200

    added a few tests for wrap identifier and terminal replace target

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/core/WrapIdentifierTest.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/core/WrapIdentifierTest.java
@@ -4,11 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import io.github.douira.glsl_transformer.TestWithTransformationManager;
-import io.github.douira.glsl_transformer.core.*;
-import io.github.douira.glsl_transformer.transform.NonFixedJobParameters;
-import io.github.douira.glsl_transformer.tree.TreeMember;
+import io.github.douira.glsl_transformer.TestForExecutionOrder;
+import io.github.douira.glsl_transformer.transform.*;
 
-public class WrapIdentifierTest extends TestWithTransformationManager<NonFixedJobParameters> {
-  
+public class WrapIdentifierTest extends TestForExecutionOrder {
+  @Test
+  void testThrowOnUnconfigured() {
+    manager.addConcurrent(new WrapIdentifier<>());
+    assertThrows(IllegalStateException.class, () -> {
+      manager.transform("");
+    }, "It should throw if basic configuration values are missing");
+  }
+
+  @Test
+  void testBasicWrap() {
+    manager.addConcurrent(new WrapIdentifier<NonFixedJobParameters>()
+        .wrapTarget("foo")
+        .detectionResult("bar")
+        .injectionExternalDeclaration("int snap = 0;")
+        .injectionLocation(InjectionPoint.BEFORE_EOF));
+    assertEquals(
+        "int bar = 4;int snap = 0;",
+        manager.transform("int foo = 4;"),
+        "It should wrap the identifier foo, replace it with bar and inject a declaration");
+  }
 }
