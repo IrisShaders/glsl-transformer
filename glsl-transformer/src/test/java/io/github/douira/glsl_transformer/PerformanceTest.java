d4859c7e8c258b74be8919a778058a6af57e6b4a
douira <douira100@gmail.com>
2022-06-12 05:00:32 +0200

    added long expression parsing test

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/PerformanceTest.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/PerformanceTest.java
@@ -7,7 +7,7 @@ import java.util.stream.*;
 
 import org.junit.jupiter.api.Test;
 
-import io.github.douira.glsl_transformer.TestResourceManager.DirectoryLocation;
+import io.github.douira.glsl_transformer.TestResourceManager.*;
 import io.github.douira.glsl_transformer.transform.*;
 
 public class PerformanceTest extends TestWithBareTransformationManager {
@@ -25,5 +25,18 @@ public class PerformanceTest extends TestWithBareTransformationManager {
     assertTimeout(Duration.ofMillis(1000),
         () -> resources.forEach(
             resource -> man.parse(resource.content())));
+  }
+
+  @Test
+  void testParsingPerformanceSingle() {
+    var man = new TransformationManager<NonFixedJobParameters>(false);
+    man.setSLLOnly();
+
+    // warmup the JVM and the parser's DFA cache
+    var input = TestResourceManager.getResource(FileLocation.LONG_EXPRESSION_TEST).content();
+    man.parse(input);
+
+    assertTimeout(Duration.ofMillis(50),
+        () -> man.parse(input));
   }
 }
