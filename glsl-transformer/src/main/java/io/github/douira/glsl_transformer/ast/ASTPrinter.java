32347464c58cf396e6d6764d3c59e20a30a2efde
douira <douira100@gmail.com>
2022-06-18 17:49:41 +0200

    split printer util

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/ASTPrinter.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/ASTPrinter.java
@@ -2,74 +2,8 @@ package io.github.douira.glsl_transformer.ast;
 
 import io.github.douira.glsl_transformer.GLSLLexer;
 import io.github.douira.glsl_transformer.ast.node.*;
-import io.github.douira.glsl_transformer.print.filter.TokenChannel;
 
-public abstract class ASTPrinter extends ASTListenerVisitor<Void> {
-  protected ASTPrinter() {
-    super();
-  }
-
-  protected abstract String generateString();
-
-  protected abstract void emitToken(PrintToken token);
-
-  public static String printAST(ASTPrinter printer, ASTNode node) {
-    printer.visit(node);
-    return printer.generateString();
-  }
-
-  public static String printSimple(ASTNode node) {
-    return printAST(new SimpleASTPrinter(), node);
-  }
-
-  protected void emitTokens(PrintToken... token) {
-    for (PrintToken t : token) {
-      emitToken(t);
-    }
-  }
-
-  protected void emitLiteral(ASTNode source, String literal) {
-    emitToken(new LiteralToken(source, literal));
-  }
-
-  protected void emitLiterals(ASTNode source, String... literal) {
-    for (String l : literal) {
-      emitLiteral(source, l);
-    }
-  }
-
-  protected void emitType(ASTNode source, int type) {
-    emitToken(new ParserToken(source, type));
-  }
-
-  protected void emitType(ASTNode source, int... type) {
-    for (int t : type) {
-      emitType(source, t);
-    }
-  }
-
-  protected void emitWhitespace(ASTNode source, String whitespace) {
-    emitToken(new LiteralToken(source, TokenChannel.WHITESPACE, whitespace));
-  }
-
-  protected void emitSpace(ASTNode source) {
-    emitWhitespace(source, " ");
-  }
-
-  protected void emitNewline(ASTNode source) {
-    emitWhitespace(source, "\n");
-  }
-
-  @Override
-  public Void initialResult() {
-    return null;
-  }
-
-  @Override
-  public Void defaultResult() {
-    throw new IllegalStateException("The default value should never be used and all nodes should be printed properly!");
-  }
-
+public abstract class ASTPrinter extends ASTPrinterUtil {
   @Override
   public void exitTranslationUnit(TranslationUnit node) {
     emitToken(new EOFToken(node));
---
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/ASTPrinterUtil.java
@@ -0,0 +1,71 @@
+package io.github.douira.glsl_transformer.ast;
+
+import io.github.douira.glsl_transformer.print.filter.TokenChannel;
+
+public abstract class ASTPrinterUtil extends ASTListenerVisitor<Void> {
+  protected ASTPrinterUtil() {
+    super();
+  }
+
+  protected abstract String generateString();
+
+  protected abstract void emitToken(PrintToken token);
+
+  public static String printAST(ASTPrinter printer, ASTNode node) {
+    printer.visit(node);
+    return printer.generateString();
+  }
+
+  public static String printSimple(ASTNode node) {
+    return printAST(new SimpleASTPrinter(), node);
+  }
+
+  protected void emitTokens(PrintToken... token) {
+    for (PrintToken t : token) {
+      emitToken(t);
+    }
+  }
+
+  protected void emitLiteral(ASTNode source, String literal) {
+    emitToken(new LiteralToken(source, literal));
+  }
+
+  protected void emitLiterals(ASTNode source, String... literal) {
+    for (String l : literal) {
+      emitLiteral(source, l);
+    }
+  }
+
+  protected void emitType(ASTNode source, int type) {
+    emitToken(new ParserToken(source, type));
+  }
+
+  protected void emitType(ASTNode source, int... type) {
+    for (int t : type) {
+      emitType(source, t);
+    }
+  }
+
+  protected void emitWhitespace(ASTNode source, String whitespace) {
+    emitToken(new LiteralToken(source, TokenChannel.WHITESPACE, whitespace));
+  }
+
+  protected void emitSpace(ASTNode source) {
+    emitWhitespace(source, " ");
+  }
+
+  protected void emitNewline(ASTNode source) {
+    emitWhitespace(source, "\n");
+  }
+
+  @Override
+  public Void initialResult() {
+    return null;
+  }
+
+  @Override
+  public Void defaultResult() {
+    throw new IllegalStateException("The default value should never be used and all nodes should be printed properly!");
+  }
+
+}
