8be4a92d30a6996090e9d80427ef839493aa33a1
douira <douira100@gmail.com>
2022-06-18 14:52:15 +0200

    added reprint test

---
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/ast/ASTIntegrationTest.java
@@ -0,0 +1,24 @@
+package io.github.douira.glsl_transformer.ast;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.douira.glsl_transformer.transform.TransformationManager;
+
+public class ASTIntegrationTest {
+  public void assertReprint(String expected, String input) {
+    var manager = new TransformationManager<>();
+    var ast = ASTBuilder.build(manager.parse(input));
+    assertEquals(expected, ASTPrinter.printSimple(ast));
+  }
+
+  public void assertReprint(String input) {
+    assertReprint(input, input);
+  }
+
+  @Test
+  public void testASTIntegration() {
+    assertReprint("#version 330 core\n;");
+  }
+}
