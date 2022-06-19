d72cbbd23497f85e2abaa98f5d2e0cd36ce15684
douira <douira100@gmail.com>
2022-06-19 21:14:33 +0200

    improve child node list

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/ChildNodeList.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/ChildNodeList.java
@@ -1,14 +1,43 @@
 package io.github.douira.glsl_transformer.ast;
 
+import java.util.Collection;
+import java.util.function.Consumer;
+
 public class ChildNodeList<Child extends ASTNode> extends ProxyArrayList<Child> {
-  private InnerASTNode parent;
+  private Consumer<Child> childConsumer;
 
   public ChildNodeList(InnerASTNode parent) {
-    this.parent = parent;
+    this(consumerFromParent(parent));
+  }
+
+  public ChildNodeList(int initialCapacity, InnerASTNode parent) {
+    this(initialCapacity, consumerFromParent(parent));
+  }
+
+  public ChildNodeList(Collection<? extends Child> c, InnerASTNode parent) {
+    this(c, consumerFromParent(parent));
+  }
+
+  public ChildNodeList(Collection<? extends Child> c, Consumer<Child> childConsumer) {
+    super(c);
+    this.childConsumer = childConsumer;
+  }
+
+  public ChildNodeList(Consumer<Child> childConsumer) {
+    this.childConsumer = childConsumer;
+  }
+
+  public ChildNodeList(int initialCapacity, Consumer<Child> childConsumer) {
+    super(initialCapacity);
+    this.childConsumer = childConsumer;
+  }
+
+  private static <T extends ASTNode> Consumer<T> consumerFromParent(InnerASTNode parent) {
+    return added -> added.setParent(parent);
   }
 
   @Override
   protected void notifyAddition(Child added) {
-    added.setParent(parent);
+    childConsumer.accept(added);
   }
 }
