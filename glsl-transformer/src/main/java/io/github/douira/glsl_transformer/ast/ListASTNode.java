2a3b4e030499992b79509200b83bafb82adb97b8
douira <douira100@gmail.com>
2022-06-19 20:38:39 +0200

    added list node interface

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/ListASTNode.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/ListASTNode.java
@@ -2,10 +2,17 @@ package io.github.douira.glsl_transformer.ast;
 
 import java.util.List;
 
-public abstract class ListASTNode<Child extends ASTNode> extends InnerASTNode {
+public abstract class ListASTNode<Child extends ASTNode>
+    extends InnerASTNode
+    implements ListNode<Child> {
   public List<Child> children = new ChildNodeList<>(this);
 
   public ListASTNode(List<Child> children) {
     this.children = children;
+  }
+
+  @Override
+  public List<Child> getChildren() {
+    return children;
   }
 }
