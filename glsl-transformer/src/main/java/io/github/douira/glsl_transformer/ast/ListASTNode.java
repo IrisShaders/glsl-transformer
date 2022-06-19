4791aec740e910bcace8da249abd063d9b916d49
douira <douira100@gmail.com>
2022-06-19 21:14:33 +0200

    improve child node list

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/ListASTNode.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/ListASTNode.java
@@ -5,10 +5,10 @@ import java.util.List;
 public abstract class ListASTNode<Child extends ASTNode>
     extends InnerASTNode
     implements ListNode<Child> {
-  public List<Child> children = new ChildNodeList<>(this);
+  public List<Child> children;
 
   public ListASTNode(List<Child> children) {
-    this.children = children;
+    this.children = new ChildNodeList<>(this);
   }
 
   @Override
