319b102370a994df296a20474f21218f46cd65eb
douira <douira100@gmail.com>
2022-06-21 19:09:22 +0200

    move things between base and general visitor

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/traversal/GeneralASTVisitor.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/traversal/GeneralASTVisitor.java
@@ -3,15 +3,26 @@ package io.github.douira.glsl_transformer.ast.traversal;
 import io.github.douira.glsl_transformer.ast.*;
 
 public interface GeneralASTVisitor<R> {
-  R startVisit(ASTNode node);
+  default R startVisit(ASTNode node) {
+    return visit(node);
+  };
 
   R visit(ASTNode node);
 
-  R visitSafe(R previousResult, ASTNode node);
+  default R visitSafe(R previousResult, ASTNode node) {
+    return node == null ? previousResult : aggregateResult(previousResult, visit(node));
+  };
 
-  R visitChildren(R previousResult, ListNode<? extends ASTNode> node);
+  default R visitChildren(R previousResult, ListNode<? extends ASTNode> node) {
+    for (var child : node.getChildren()) {
+      previousResult = visitSafe(previousResult, child);
+    }
+    return previousResult;
+  };
 
-  R visitChildren(ListNode<? extends ASTNode> node);
+  default R visitChildren(ListNode<? extends ASTNode> node) {
+    return visitChildren(initialResult(), node);
+  };
 
   R initialResult();
 
