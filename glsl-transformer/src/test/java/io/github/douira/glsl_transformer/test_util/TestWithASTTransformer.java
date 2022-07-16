ae853352ab81606962880f315e6f907ee0b47e1d
douira <douira100@gmail.com>
2022-07-16 02:12:33 +0200

    added job parameters and test for it

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/test_util/TestWithASTTransformer.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/test_util/TestWithASTTransformer.java
@@ -3,12 +3,13 @@ package io.github.douira.glsl_transformer.test_util;
 import org.junit.jupiter.api.BeforeEach;
 
 import io.github.douira.glsl_transformer.ast.transform.ASTTransformer;
+import io.github.douira.glsl_transformer.job_parameter.JobParameters;
 
 public abstract class TestWithASTTransformer {
-  public ASTTransformer transformer;
+  public ASTTransformer<JobParameters> transformer;
 
   @BeforeEach
   public void setUp() {
-    transformer = new ASTTransformer();
+    transformer = new ASTTransformer<>();
   }
 }
