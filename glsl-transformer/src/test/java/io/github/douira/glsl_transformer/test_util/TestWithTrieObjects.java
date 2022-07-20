404dac1dfb5b41fcf3440ccd11e1eb3c00a1d0a8
douira <douira100@gmail.com>
2022-07-20 04:00:37 +0200

    fix tests

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/test_util/TestWithTrieObjects.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/test_util/TestWithTrieObjects.java
@@ -1,12 +1,10 @@
 package io.github.douira.glsl_transformer.test_util;
 
-import java.util.*;
-
 public abstract class TestWithTrieObjects {
-  public Set<Object> a = new HashSet<>(),
-      b = new HashSet<>(),
-      c = new HashSet<>(),
-      d = new HashSet<>(),
-      e = new HashSet<>(),
-      f = new HashSet<>();
+  public Object a = new Object(),
+      b = new Object(),
+      c = new Object(),
+      d = new Object(),
+      e = new Object(),
+      f = new Object();
 }
