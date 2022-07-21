9c9587a131263828a32c9035d778b5277b5b8526
douira <douira100@gmail.com>
2022-07-21 02:44:08 +0200

    formatting, make context tracker non-required

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/traversal/ContextTracker.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/traversal/ContextTracker.java
@@ -3,5 +3,6 @@ package io.github.douira.glsl_transformer.ast.traversal;
 import io.github.douira.glsl_transformer.ast.node.basic.ASTNode;
 
 public interface ContextTracker {
-  void enterContext(ASTNode node);
+  default void enterContext(ASTNode node) {
+  }
 }
