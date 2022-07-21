08520748ee4f3cf0fbe9608bfed4f36eeb000f05
douira <douira100@gmail.com>
2022-07-21 15:54:50 +0200

    fix rename all

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/ast/query/IdentifierIndexTest.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/ast/query/IdentifierIndexTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import io.github.douira.glsl_transformer.ast.node.Identifier;
 import io.github.douira.glsl_transformer.test_util.TestWithASTTransformer;
 
 public class IdentifierIndexTest extends TestWithASTTransformer {
@@ -17,5 +18,23 @@ public class IdentifierIndexTest extends TestWithASTTransformer {
       assertTrue(root.identifierIndex.has("b"));
     });
     transformer.transform("int a = 1;");
+  }
+
+  @Test
+  void testRenameAll() {
+    Root.indexSeparateTrees(register -> {
+      var a = new Identifier("a");
+      var b = new Identifier("b");
+      register.apply(a);
+      register.apply(b);
+      var index = a.getRoot().identifierIndex;
+      assertTrue(index.has("a"));
+      assertTrue(index.has("b"));
+      assertFalse(index.has("c"));
+      index.renameAll("a", "c");
+      assertFalse(index.has("a"));
+      assertTrue(index.has("b"));
+      assertTrue(index.has("c"));
+    });
   }
 }
