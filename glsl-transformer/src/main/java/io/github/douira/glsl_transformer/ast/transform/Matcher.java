3fb5fe0b47f9c30c23c3902020c16f89d80ba892
douira <douira100@gmail.com>
2022-07-22 03:48:16 +0200

    added advanced pattern matching to Matcher

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/transform/Matcher.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/transform/Matcher.java
@@ -1,7 +1,7 @@
 package io.github.douira.glsl_transformer.ast.transform;
 
 import java.util.*;
-import java.util.function.Function;
+import java.util.function.*;
 
 import io.github.douira.glsl_transformer.GLSLParser;
 import io.github.douira.glsl_transformer.ast.node.basic.ASTNode;
@@ -14,49 +14,123 @@ import io.github.douira.glsl_transformer.tree.ExtendedContext;
  * avoids a separate equality implementation for each node type.
  */
 public class Matcher {
-  private final ASTNode pattern;
+  protected final ASTNode pattern;
+  private final String wildcardPrefix;
+  private Map<String, Object> dataMatches;
+  private Map<String, ASTNode> nodeMatches;
+  private Map<ASTNode, NodeWildcard> nodeWildcards;
+  private boolean collectMatches = false;
   private List<Object> patternItems;
   private int patternItemsSize;
   private int matchIndex;
   private boolean matches;
 
-  public Matcher(ASTNode pattern) {
+  public Matcher(ASTNode pattern, String wildcardIdentifier) {
     this.pattern = pattern;
+    this.wildcardPrefix = wildcardIdentifier;
+  }
+
+  public Matcher(ASTNode pattern) {
+    this(pattern, null);
+  }
+
+  public Matcher(String input,
+      Function<GLSLParser, ? extends ExtendedContext> parseMethod,
+      String wildcardIdentifier) {
+    this(ASTBuilder.build(
+        EnhancedParser.getInternalInstance().parse(input, parseMethod)), wildcardIdentifier);
   }
 
   public Matcher(String input,
       Function<GLSLParser, ? extends ExtendedContext> parseMethod) {
-    this(ASTBuilder.build(
-        EnhancedParser.getInternalInstance().parse(input, parseMethod)));
+    this(input, parseMethod, null);
   }
 
   private ASTVisitor<?> matchVisitor = new ASTVoidVisitor() {
     @Override
-    public void visitVoid(ASTNode node) {
-      // ASTNodes have to match by exact class (the type of the node)
-      matches = matches
-          && matchIndex < patternItemsSize
-          && node.getClass() == patternItems.get(matchIndex++).getClass();
+    public Void visit(ASTNode node) {
+      if (!matches || matchIndex >= patternItemsSize) {
+        matches = false;
+        return null;
+      }
+      var patternItem = patternItems.get(matchIndex++);
+
+      // match either a wildcard node
+      if (collectMatches
+          && nodeWildcards != null
+          && patternItem instanceof NodeWildcard wildcard
+          && wildcard.test(node)) {
+        nodeMatches.put(wildcard.name, node);
+        return null;
+      }
+
+      // or the class exactly
+      if (node.getClass() != patternItem.getClass()) {
+        matches = false;
+        return null;
+      }
+      return node.accept(this);
     }
 
     @Override
     public void visitVoid(Object data) {
       // Data has to only match by equality since these might also be strings.
-      matches = matches
-          && matchIndex < patternItemsSize
-          && !(data instanceof ASTNode)
-          && data.equals(patternItems.get(matchIndex++));
-    }
-
-    @Override
-    public Void visit(ASTNode node) {
-      // stop visiting when the match has failed
-      if (!matches) {
-        return null;
+      if (!matches
+          || matchIndex >= patternItemsSize
+          || data instanceof ASTNode) {
+        matches = false;
+        return;
       }
-      return super.visit(node);
+      var patternItem = patternItems.get(matchIndex++);
+
+      // match either a wildcard or a data item exactly
+      if (wildcardPrefix != null
+          && patternItem instanceof String str
+          && str.startsWith(wildcardPrefix)) {
+        // the wildcard was matched, but the string is only collected if enabled
+        if (collectMatches) {
+          dataMatches.put(str.substring(wildcardPrefix.length()), data);
+        }
+        return;
+      }
+
+      if (!Objects.equals(data, patternItem)) {
+        matches = false;
+        return;
+      }
     }
   };
+
+  public void preparePatternItems() {
+    if (patternItems != null) {
+      return;
+    }
+
+    // visit the pattern and collect all items (ASTNodes and data),
+    // don't collect subtrees of node wildcards
+    patternItems = new ArrayList<>();
+    new ASTVoidVisitor() {
+      @Override
+      public Void visit(ASTNode node) {
+        if (nodeWildcards != null) {
+          var wildcard = nodeWildcards.get(node);
+          if (wildcard != null) {
+            patternItems.add(wildcard);
+            return null;
+          }
+        }
+        patternItems.add(node);
+        node.accept(this);
+        return null;
+      }
+
+      @Override
+      public void visitVoid(Object data) {
+        patternItems.add(data);
+      }
+    }.startVisit(pattern);
+    patternItemsSize = patternItems.size();
+  }
 
   /**
    * Traverse the given tree and the pattern at the same time and make sure they
@@ -73,21 +147,171 @@ public class Matcher {
     return matches;
   }
 
-  public void preparePatternItems() {
-    if (patternItems == null) {
-      patternItems = new ArrayList<>();
-      new ASTVoidVisitor() {
-        @Override
-        public void visitVoid(ASTNode node) {
-          patternItems.add(node);
-        }
-
-        @Override
-        public void visitVoid(Object data) {
-          patternItems.add(data);
-        }
-      }.startVisit(pattern);
-      patternItemsSize = patternItems.size();
+  private void ensureMatchMaps() {
+    if (dataMatches == null) {
+      dataMatches = new HashMap<>();
     }
+    if (nodeMatches == null) {
+      nodeMatches = new HashMap<>();
+    }
+  }
+
+  public boolean matchesExtract(ASTNode tree) {
+    ensureMatchMaps();
+    dataMatches.clear();
+    nodeMatches.clear();
+    collectMatches = true;
+    var succeeded = matches(tree);
+    collectMatches = false;
+    if (!succeeded) {
+      dataMatches.clear();
+      nodeMatches.clear();
+    }
+    return succeeded;
+  }
+
+  public boolean matchesExtract(
+      ASTNode tree,
+      Map<String, Object> dataMatches,
+      Map<String, ASTNode> nodeMatches) {
+    this.dataMatches = dataMatches;
+    this.nodeMatches = nodeMatches;
+    var succeeded = matchesExtract(tree);
+    this.dataMatches = null;
+    this.nodeMatches = null;
+    return succeeded;
+  }
+
+  public Map<String, Object> getDataMatches() {
+    return dataMatches;
+  }
+
+  public Map<String, ASTNode> getNodeMatches() {
+    return nodeMatches;
+  }
+
+  public Object getDataMatch(String name) {
+    return dataMatches.get(name);
+  }
+
+  public String getStringDataMatch(String name) {
+    var result = dataMatches.get(name);
+    return result instanceof String str ? str : null;
+  }
+
+  public ASTNode getNodeMatch(String name) {
+    return nodeMatches.get(name);
+  }
+
+  public <T extends ASTNode> T getNodeMatch(String name, Class<T> clazz) {
+    var result = nodeMatches.get(name);
+    return clazz.isInstance(result) ? clazz.cast(result) : null;
+  }
+
+  private void ensureWildcardMap() {
+    if (nodeWildcards == null) {
+      nodeWildcards = new HashMap<>();
+    }
+  }
+
+  private static abstract class NodeWildcard implements Predicate<ASTNode> {
+    final String name;
+
+    NodeWildcard(String name) {
+      this.name = name;
+    }
+  }
+
+  private void markWildcard(ASTNode node, NodeWildcard wildcard) {
+    ensureWildcardMap();
+    nodeWildcards.put(node, wildcard);
+  }
+
+  private static class AnyWildcard extends NodeWildcard {
+    AnyWildcard(String name) {
+      super(name);
+    }
+
+    @Override
+    public boolean test(ASTNode node) {
+      return true;
+    }
+  }
+
+  public void markAnyWildcard(String name, ASTNode patternNode) {
+    markWildcard(patternNode, new AnyWildcard(name));
+  }
+
+  private static class PredicateWildcard extends NodeWildcard {
+    final Predicate<ASTNode> predicate;
+
+    PredicateWildcard(String name, Predicate<ASTNode> predicate) {
+      super(name);
+      this.predicate = predicate;
+    }
+
+    @Override
+    public boolean test(ASTNode node) {
+      return predicate.test(node);
+    }
+  }
+
+  public void markPredicatedWildcard(
+      String name,
+      ASTNode patternNode,
+      Predicate<ASTNode> matchPredicate) {
+    markWildcard(patternNode, new PredicateWildcard(name, matchPredicate));
+  }
+
+  private static class ClassWildcard extends NodeWildcard {
+    final Class<? extends ASTNode> clazz;
+
+    ClassWildcard(String name, Class<? extends ASTNode> clazz) {
+      super(name);
+      this.clazz = clazz;
+    }
+
+    @Override
+    public boolean test(ASTNode node) {
+      return clazz.isInstance(node);
+    }
+  }
+
+  public void markClassWildcard(
+      String name,
+      ASTNode patternNode,
+      Class<? extends ASTNode> clazz) {
+    markWildcard(patternNode, new ClassWildcard(name, clazz));
+  }
+
+  public void markClassWildcard(
+      String name,
+      ASTNode patternNode) {
+    ensureWildcardMap();
+    nodeWildcards.put(patternNode, new ClassWildcard(name, patternNode.getClass()));
+  }
+
+  private static class ClassedPredicateWildcard<T extends ASTNode> extends NodeWildcard {
+    final Class<T> clazz;
+    final Predicate<T> predicate;
+
+    ClassedPredicateWildcard(String name, Class<T> clazz, Predicate<T> predicate) {
+      super(name);
+      this.clazz = clazz;
+      this.predicate = predicate;
+    }
+
+    @Override
+    public boolean test(ASTNode node) {
+      return clazz.isInstance(node) && predicate.test(clazz.cast(node));
+    }
+  }
+
+  public <T extends ASTNode> void markClassedPredicateWildcard(
+      String name,
+      ASTNode patternNode,
+      Class<T> clazz,
+      Predicate<T> predicate) {
+    markWildcard(patternNode, new ClassedPredicateWildcard<>(name, clazz, predicate));
   }
 }
--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/ast/transform/MatcherTest.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/ast/transform/MatcherTest.java
@@ -4,41 +4,223 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.function.Function;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import io.github.douira.glsl_transformer.GLSLParser;
+import io.github.douira.glsl_transformer.ast.node.*;
+import io.github.douira.glsl_transformer.ast.node.basic.InnerASTNode;
+import io.github.douira.glsl_transformer.ast.node.expression.*;
+import io.github.douira.glsl_transformer.ast.node.expression.binary.AdditionExpression;
+import io.github.douira.glsl_transformer.ast.node.expression.unary.FunctionCallExpression;
+import io.github.douira.glsl_transformer.ast.node.external_declaration.*;
 import io.github.douira.glsl_transformer.ast.node.type.qualifier.PreciseQualifier;
 import io.github.douira.glsl_transformer.test_util.TestWithASTTransformer;
 import io.github.douira.glsl_transformer.tree.ExtendedContext;
 
 public class MatcherTest extends TestWithASTTransformer {
-  private void assertMatch(
-      Matcher matcher,
-      String input,
-      Function<GLSLParser, ? extends ExtendedContext> parseMethod) {
-    assertTrue(matcher.matches(
-        transformer.parseNodeWithoutRoot(input, parseMethod)));
+  Function<GLSLParser, ? extends ExtendedContext> parseMethod;
+  Matcher p;
+
+  @BeforeEach
+  void setup() {
+    parseMethod = null;
+    p = null;
   }
 
-  private void assertNoMatch(
-      Matcher matcher,
-      String input,
-      Function<GLSLParser, ? extends ExtendedContext> parseMethod) {
-    assertFalse(matcher.matches(
-        transformer.parseNodeWithoutRoot(input, parseMethod)));
+  private void assertNoMatch(String input) {
+    assertFalse(p.matches(transformer.parseNode(input, parseMethod)));
+  }
+
+  private void assertMatch(String input) {
+    assertTrue(p.matches(transformer.parseNode(input, parseMethod)));
+  }
+
+  private void assertNoExtract(String input) {
+    assertFalse(p.matchesExtract(transformer.parseNode(input, parseMethod)));
+  }
+
+  private void assertExtract(String input) {
+    assertTrue(p.matchesExtract(transformer.parseNode(input, parseMethod)));
   }
 
   @Test
   void testMatches() {
-    var p = new Matcher("uniform vec4 entityColor;",
-        GLSLParser::externalDeclaration);
+    parseMethod = GLSLParser::externalDeclaration;
+    p = new Matcher("uniform vec4 entityColor;", parseMethod);
+
     assertFalse(p.matches(null));
     assertFalse(p.matches(new PreciseQualifier()));
-    assertMatch(p, "uniform vec4 entityColor;", GLSLParser::externalDeclaration);
-    assertNoMatch(p, "uniform vec3 entityColor;", GLSLParser::externalDeclaration);
-    assertNoMatch(p, "in vec4 entityColor;", GLSLParser::externalDeclaration);
-    assertNoMatch(p, "uniform precise vec4 entityColor;", GLSLParser::externalDeclaration);
-    assertNoMatch(p, "uniform vec4 entityColo;", GLSLParser::externalDeclaration);
-    assertNoMatch(p, "uniform vec4 entityColor_;", GLSLParser::externalDeclaration);
+    assertMatch("uniform vec4 entityColor;");
+    assertNoMatch("uniform vec3 entityColor;");
+    assertNoMatch("in vec4 entityColor;");
+    assertNoMatch("uniform precise vec4 entityColor;");
+    assertNoMatch("uniform vec4 entityColo;");
+    assertNoMatch("uniform vec4 entityColor_;");
+  }
+
+  @Test
+  void testMatchesDataWildcard() {
+    parseMethod = GLSLParser::externalDeclaration;
+    p = new Matcher("uniform vec4 ___ = foo + ___;",
+        parseMethod, "___");
+
+    assertFalse(p.matches(null));
+    assertFalse(p.matches(new PreciseQualifier()));
+    assertMatch("uniform vec4 foo = foo + foo;");
+    assertNoMatch("uniform vec3 foo = foo + foo;");
+    assertNoMatch("in vec4 foo = foo + foo;");
+    assertNoMatch("uniform precise vec4 foo = foo + foo;");
+    assertNoMatch("uniform vec4 foo = bar + foo;");
+    assertNoMatch("uniform vec4 foo = __ + foo;");
+  }
+
+  @Test
+  void testExtractDataWildcards() {
+    parseMethod = GLSLParser::externalDeclaration;
+    p = new Matcher("uniform vec4 ___a = foo + ___b;",
+        parseMethod, "___");
+
+    assertFalse(p.matchesExtract(null));
+    assertTrue(p.getDataMatches().isEmpty());
+    assertTrue(p.getNodeMatches().isEmpty());
+
+    assertFalse(p.matchesExtract(new PreciseQualifier()));
+    assertTrue(p.getDataMatches().isEmpty());
+    assertTrue(p.getNodeMatches().isEmpty());
+
+    assertExtract("uniform vec4 bar = foo + bam;");
+    var dataMatches = p.getDataMatches();
+    assertEquals("bar", dataMatches.get("a"));
+    assertEquals("bam", dataMatches.get("b"));
+    assertEquals(dataMatches.get("b"), p.getDataMatch("b"));
+    assertEquals(dataMatches.get("b"), p.getStringDataMatch("b"));
+    assertTrue(p.getNodeMatches().isEmpty());
+
+    assertNoMatch("uniform vec4 bar = foo;");
+    // It should not match Identifiers with LiteralExpressions
+    assertNoMatch("uniform vec4 bar = foo + 5;");
+  }
+
+  @Test
+  void testExtractNodeWildcards() {
+    parseMethod = GLSLParser::externalDeclaration;
+    p = new Matcher("uniform vec4 a = foo + b;", parseMethod) {
+      {
+        markAnyWildcard("aNode",
+            pattern.getRoot().identifierIndex.getOne("a"));
+        markAnyWildcard("bNode",
+            pattern.getRoot().identifierIndex.getOne("b")
+                .getAncestor(ReferenceExpression.class));
+      }
+    };
+
+    assertExtract("uniform vec4 bam = foo + bar;");
+    assertTrue(p.getDataMatches().isEmpty());
+    assertEquals("bam",
+        p.getNodeMatch("aNode", Identifier.class).getName());
+    assertEquals("bar",
+        p.getNodeMatch("bNode", ReferenceExpression.class)
+            .getIdentifier().getName());
+
+    assertExtract("uniform vec4 bam = foo + a();");
+    assertTrue(p.getDataMatches().isEmpty());
+    assertNotNull(p.getNodeMatch("bNode", FunctionCallExpression.class));
+
+    assertExtract("uniform vec4 bam = foo + 5;");
+    assertEquals(5,
+        p.getNodeMatch("bNode", LiteralExpression.class).getNumber());
+    assertExtract("uniform vec4 bam = foo + 5f;");
+    assertEquals(5f,
+        p.getNodeMatch("bNode", LiteralExpression.class).getNumber());
+  }
+
+  @Test
+  void testExtractNodeWildcardsList() {
+    parseMethod = GLSLParser::translationUnit;
+    p = new Matcher("int ___a; int x = a; b; float ___b;", parseMethod, "___") {
+      {
+        markAnyWildcard("aNode", pattern.getRoot().identifierIndex
+            .getOne("a").getAncestor(Expression.class));
+        markAnyWildcard("bNode", pattern.getAncestor(
+            TranslationUnit.class).getChildren().get(2));
+      }
+    };
+
+    assertExtract("int foo; int x = 45 + 5; void main() {} float bar;");
+    assertEquals("foo", p.getStringDataMatch("a"));
+    assertEquals("bar", p.getStringDataMatch("b"));
+    assertEquals(AdditionExpression.class,
+        p.getNodeMatch("aNode").getClass());
+    assertEquals("main",
+        p.getNodeMatch("bNode", FunctionDefinition.class)
+            .getFunctionPrototype().getName().getName());
+  }
+
+  @Test
+  void testPredicatedWildcard() {
+    parseMethod = GLSLParser::translationUnit;
+    p = new Matcher("a;", parseMethod) {
+      {
+        markPredicatedWildcard("bNode",
+            pattern.getAncestor(TranslationUnit.class)
+                .getChildren().get(0),
+            node -> node instanceof EmptyDeclaration);
+      }
+    };
+
+    assertNoExtract("foo;");
+    assertExtract(";");
+  }
+
+  @Test
+  void testClassWildcard1() {
+    parseMethod = GLSLParser::translationUnit;
+    p = new Matcher("a;", parseMethod) {
+      {
+        markClassWildcard("bNode",
+            pattern.getAncestor(TranslationUnit.class)
+                .getChildren().get(0),
+            EmptyDeclaration.class);
+      }
+    };
+
+    assertNoExtract("foo;");
+    assertExtract(";");
+  }
+
+  @Test
+  void testClassWildcard2() {
+    parseMethod = GLSLParser::translationUnit;
+    p = new Matcher("a;", parseMethod) {
+      {
+        markClassWildcard("bNode",
+            pattern.getAncestor(TranslationUnit.class)
+                .getChildren().get(0),
+            InnerASTNode.class);
+      }
+    };
+
+    assertExtract("foo;");
+    assertExtract(";");
+  }
+
+  @Test
+  void testClassedPredicateWildcard() {
+    parseMethod = GLSLParser::translationUnit;
+    p = new Matcher("a;", parseMethod) {
+      {
+        markClassedPredicateWildcard("bNode",
+            pattern.getAncestor(TranslationUnit.class)
+                .getChildren().get(0),
+            FunctionDefinition.class,
+            functionCall -> functionCall.getFunctionPrototype()
+                .getName().getName().equals("main"));
+      }
+    };
+
+    assertNoExtract("foo;");
+    assertNoExtract(";");
+    assertExtract("void main() {}");
+    assertNoExtract("void foo() {}");
   }
 }
