2729b409e300b37ef3317bbac5067d3392e364fe
douira <douira100@gmail.com>
2022-07-23 16:50:43 +0200

    added superclass node index for querying nodes by their relevant AST superclasses

---
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/query/SuperclassNodeIndex.java
@@ -0,0 +1,66 @@
+package io.github.douira.glsl_transformer.ast.query;
+
+import java.util.*;
+import java.util.function.*;
+
+import io.github.douira.glsl_transformer.ast.node.basic.*;
+
+/**
+ * The superclass node index also creates index entries for the superclasses for
+ * all nodes. This means querying for Expression returns all Expressions.
+ */
+public class SuperclassNodeIndex extends NodeIndex {
+  public SuperclassNodeIndex(Supplier<Set<ASTNode>> bucketConstructor) {
+    super(bucketConstructor);
+  }
+
+  public SuperclassNodeIndex() {
+  }
+
+  public static SuperclassNodeIndex withHashSetBuckets() {
+    return new SuperclassNodeIndex(HashSet::new);
+  }
+
+  public static SuperclassNodeIndex withLinkedHashSetBuckets() {
+    return new SuperclassNodeIndex(LinkedHashSet::new);
+  }
+
+  @SuppressWarnings("unchecked")
+  protected void iterateClasses(
+      ASTNode node,
+      BiConsumer<Class<? extends ASTNode>, ASTNode> consumer) {
+    Class<? extends ASTNode> clazz = node.getClass();
+    while (clazz != null
+        && clazz != InnerASTNode.class
+        && clazz != ASTNode.class
+        && clazz != ListASTNode.class) {
+      consumer.accept(clazz, node);
+      clazz = (Class<? extends ASTNode>) clazz.getSuperclass();
+    }
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public void add(ASTNode node) {
+    iterateClasses(node, (clazz, toAdd) -> {
+      var set = (Set<ASTNode>) index.get(clazz);
+      if (set == null) {
+        set = bucketConstructor.get();
+        index.put(clazz, set);
+      }
+      set.add(toAdd);
+    });
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public void remove(ASTNode node) {
+    iterateClasses(node, (clazz, toAdd) -> {
+      var set = (Set<ASTNode>) index.get(clazz);
+      if (set == null) {
+        return;
+      }
+      set.remove(toAdd);
+    });
+  }
+}
---
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/ast/query/SuperclassNodeIndexTest.java
@@ -0,0 +1,41 @@
+package io.github.douira.glsl_transformer.ast.query;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.*;
+
+import io.github.douira.glsl_transformer.ast.node.basic.*;
+import io.github.douira.glsl_transformer.ast.node.type.qualifier.*;
+
+public class SuperclassNodeIndexTest {
+  NodeIndex index = new SuperclassNodeIndex();
+  ASTNode a = new PreciseQualifier();
+  ASTNode b = new PreciseQualifier();
+  ASTNode c = new PreciseQualifier();
+  ASTNode d = new PreciseQualifier();
+  ASTNode e = new TypeQualifier(Stream.empty());
+
+  @BeforeEach
+  void setUp() {
+    index.index.clear();
+  }
+
+  @Test
+  void testAdd() {
+    index.add(a);
+    index.add(b);
+    index.add(c);
+    index.add(d);
+    index.add(e);
+    assertEquals(3, index.index.size());
+    assertEquals(4, index.get(a).size());
+    assertEquals(4, index.get(PreciseQualifier.class).size());
+    assertEquals(4, index.get(TypeQualifierPart.class).size());
+    assertEquals(1, index.get(TypeQualifier.class).size());
+    assertEquals(0, index.get(ASTNode.class).size());
+    assertEquals(0, index.get(InnerASTNode.class).size());
+    assertEquals(0, index.get(ListASTNode.class).size());
+  }
+}
