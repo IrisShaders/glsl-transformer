2e0ef0850251250a8c24aed2aadf55823a65c97b
douira <douira100@gmail.com>
2022-07-26 16:19:45 +0200

    move version enums outside of version statement

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/node/VersionStatement.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/node/VersionStatement.java
@@ -1,77 +1,14 @@
 package io.github.douira.glsl_transformer.ast.node;
 
-import org.antlr.v4.runtime.Token;
-
-import io.github.douira.glsl_transformer.GLSLLexer;
-import io.github.douira.glsl_transformer.ast.data.*;
 import io.github.douira.glsl_transformer.ast.node.basic.ASTNode;
 import io.github.douira.glsl_transformer.ast.traversal.ASTVisitor;
 
+/**
+ * The version statement holds information about the selected GLSL version and
+ * profile. This class' {@link Version} and {@link Profile} enums are declared
+ * outside of this class for easier use in the lexer.
+ */
 public class VersionStatement extends ASTNode {
-  public enum Profile implements TokenTyped {
-    CORE(GLSLLexer.NR_CORE),
-    COMPATIBILITY(GLSLLexer.NR_COMPATIBILITY),
-    ES(GLSLLexer.NR_ES);
-
-    public final int tokenType;
-
-    private Profile(int tokenType) {
-      this.tokenType = tokenType;
-    }
-
-    @Override
-    public int getTokenType() {
-      return tokenType;
-    }
-
-    public static Profile fromToken(Token token) {
-      return TypeUtil.enumFromToken(Profile.values(), token);
-    }
-  }
-
-  public enum Version implements TokenTyped {
-    GL20(GLSLLexer.NR_GL_110, 110),
-    GL21(GLSLLexer.NR_GL_120, 120),
-    GLES20(GLSLLexer.NR_GLES_100, 100, true),
-    GL30(GLSLLexer.NR_GL_130, 130),
-    GL31(GLSLLexer.NR_GL_140, 140),
-    GL32(GLSLLexer.NR_GL_150, 150),
-    GL33(GLSLLexer.NR_GL_330, 330),
-    GLES30(GLSLLexer.NR_GLES_300, 300, true),
-    GLES31(GLSLLexer.NR_GLES_310, 310, true),
-    GLES32(GLSLLexer.NR_GLES_320, 320, true),
-    GL40(GLSLLexer.NR_GL_400, 400),
-    GL41(GLSLLexer.NR_GL_410, 410),
-    GL42(GLSLLexer.NR_GL_420, 420),
-    GL43(GLSLLexer.NR_GL_430, 430),
-    GL44(GLSLLexer.NR_GL_440, 440),
-    GL45(GLSLLexer.NR_GL_450, 450),
-    GL46(GLSLLexer.NR_GL_460, 460);
-
-    public final int tokenType;
-    public final int version;
-    public final boolean es;
-
-    private Version(int tokenType, int version, boolean es) {
-      this.tokenType = tokenType;
-      this.version = version;
-      this.es = es;
-    }
-
-    private Version(int tokenType, int version) {
-      this(tokenType, version, false);
-    }
-
-    @Override
-    public int getTokenType() {
-      return tokenType;
-    }
-
-    public static Version fromToken(Token token) {
-      return TypeUtil.enumFromToken(Version.values(), token);
-    }
-  }
-
   public Version version;
   public Profile profile; // TODO: nullable
 
