e72cda087cc7628049ef0b0d90b3a5b1fa157a01
douira <douira100@gmail.com>
2022-07-26 20:33:07 +0200

    improve parsing performance test

--- glsl-transformer/src/test/java/io/github/douira/glsl_transformer/ParsingPerformanceTest.java
+++ glsl-transformer/src/test/java/io/github/douira/glsl_transformer/ParsingPerformanceTest.java
@@ -6,22 +6,35 @@ import java.time.Duration;
 import java.util.Collection;
 import java.util.stream.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import io.github.douira.glsl_transformer.basic.EnhancedParser;
 import io.github.douira.glsl_transformer.test_util.*;
 import io.github.douira.glsl_transformer.test_util.TestResourceManager.*;
 
 public class ParsingPerformanceTest extends TestWithBareCSTTransformer {
+  String displayName;
+
+  @BeforeEach
+  void setUp(TestInfo testInfo) {
+    displayName = testInfo.getDisplayName();
+  }
+
   private void assertPerformance(boolean throwParseErrors, Duration expected, Collection<String> inputs) {
     var parser = new EnhancedParser(throwParseErrors);
     parser.setSLLOnly();
 
     // warmup the JVM and the parser's DFA cache
     inputs.forEach(parser::parse);
-    inputs.forEach(parser::parse);
-
-    assertTimeout(expected, () -> inputs.forEach(parser::parse),
+    var n = System.getProperty("GRADLE") != null ? 5 : 30;
+    assertTimeout(expected.multipliedBy(n), () -> {
+      var start = System.nanoTime();
+      for (int i = 0; i < n; i++) {
+        inputs.forEach(parser::parse);
+      }
+      var duration = Duration.ofNanos(System.nanoTime() - start);
+      System.out.println(displayName + ": " + duration.dividedBy(n) + " (" + n + " times)");
+    },
         "It should parse fast enough using SLL mode.");
   }
 
@@ -45,13 +58,13 @@ public class ParsingPerformanceTest extends TestWithBareCSTTransformer {
   @Test
   void testParsingPerformanceGLSLang() {
     assertDirectoryPerformance(false,
-        Duration.ofMillis(300), DirectoryLocation.GLSLANG_TESTS);
+        Duration.ofMillis(700), DirectoryLocation.GLSLANG_TESTS);
   }
 
   @Test
   void testDeepStatementParsing() {
     assertFilePerformance(true,
-        Duration.ofMillis(600), FileLocation.DEEP_STATEMENT_TEST);
+        Duration.ofMillis(100), FileLocation.DEEP_STATEMENT_TEST);
   }
 
   @Test
@@ -63,18 +76,18 @@ public class ParsingPerformanceTest extends TestWithBareCSTTransformer {
   @Test
   void testDeepExpressionParsing() {
     assertFilePerformance(true,
-        Duration.ofMillis(300), FileLocation.DEEP_EXPRESSION_TEST);
+        Duration.ofMillis(100), FileLocation.DEEP_EXPRESSION_TEST);
   }
 
   @Test
   void testLongExpressionParsing() {
     assertFilePerformance(true,
-        Duration.ofMillis(300), FileLocation.LONG_EXPRESSION_TEST);
+        Duration.ofMillis(100), FileLocation.LONG_EXPRESSION_TEST);
   }
 
   @Test
   void testCommentParsing() {
     assertFilePerformance(true,
-        Duration.ofMillis(300), FileLocation.COMMENT_TEST);
+        Duration.ofMillis(100), FileLocation.COMMENT_TEST);
   }
 }
