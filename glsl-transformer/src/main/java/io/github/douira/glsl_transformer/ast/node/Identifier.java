55112a6c099a063d3da08c32ffc67cb0e55e1e5e
douira <douira100@gmail.com>
2022-07-28 16:53:30 +0200

    Identifier content validation

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/node/Identifier.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/node/Identifier.java
@@ -10,6 +10,7 @@ public class Identifier extends ASTNode {
 
   public Identifier(String name) {
     this.name = name;
+    validateContents(name);
   }
 
   public Identifier(Token token) {
@@ -24,9 +25,27 @@ public class Identifier extends ASTNode {
     if (this.name.equals(name)) {
       return;
     }
+    validateContents(name);
     getRoot().identifierIndex.remove(this);
     this.name = name;
     getRoot().identifierIndex.add(this);
+  }
+
+  private void validateContents(String str) {
+    // verify identifier contents to be only a-z, A-Z, 0-9 and _
+    // and does not start with a digit
+    if (str.length() == 0) {
+      throw new IllegalArgumentException("Identifier cannot be empty");
+    }
+    if (!Character.isLetter(str.charAt(0)) && str.charAt(0) != '_') {
+      throw new IllegalArgumentException("Identifier must start with a letter or underscore");
+    }
+    for (int i = 1; i < str.length(); i++) {
+      char c = str.charAt(i);
+      if (!(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_')) {
+        throw new IllegalArgumentException("Invalid identifier name: " + str);
+      }
+    }
   }
 
   @Override
