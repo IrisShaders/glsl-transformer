9cbe9a61cd7ba4d916bd47718d8ff5e6b2a298aa
douira <douira100@gmail.com>
2023-02-15 04:00:24 +0100

    add ed index static root supplier variant, remove some unnecessary variants

--- glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/query/RootSupplier.java
+++ glsl-transformer/src/main/java/io/github/douira/glsl_transformer/ast/query/RootSupplier.java
@@ -1,28 +1,28 @@
 package io.github.douira.glsl_transformer.ast.query;
 
-import java.util.LinkedHashSet;
 import java.util.function.Supplier;
 
 import io.github.douira.glsl_transformer.ast.query.index.*;
 
 /**
  * Supplies root instances with specific pre-configured indexes. There are a
- * number of presets which are the most common but custom instances of this
+ * number of presets which are the most common, but custom instances of this
  * class can specify their own index suppliers.
  */
 public class RootSupplier implements Supplier<Root> {
-  public static final RootSupplier EXACT_UNORDERED = new RootSupplier(NodeIndex::withUnordered,
-      IdentifierIndex::withOnlyExact);
-  public static final RootSupplier EXACT_ORDERED = new RootSupplier(NodeIndex::withOrdered,
-      IdentifierIndex::withOnlyExact);
-  public static final RootSupplier EXACT_ORDERED_BOTH = new RootSupplier(NodeIndex::withOrdered,
-      () -> IdentifierIndex.withOnlyExact(LinkedHashSet::new));
-  public static final RootSupplier PREFIX_UNORDERED = new RootSupplier(NodeIndex::withUnordered,
-      PrefixIdentifierIndex::withPrefix);
-  public static final RootSupplier PREFIX_ORDERED = new RootSupplier(NodeIndex::withOrdered,
-      PrefixIdentifierIndex::withPrefix);
-  public static final RootSupplier PREFIX_ORDERED_BOTH = new RootSupplier(NodeIndex::withOrdered,
-      () -> IdentifierIndex.withOnlyExact(LinkedHashSet::new));
+  public static final RootSupplier EXACT_UNORDERED = new RootSupplier(
+      NodeIndex::withUnordered, IdentifierIndex::withOnlyExact);
+  public static final RootSupplier PREFIX_UNORDERED = new RootSupplier(
+      NodeIndex::withUnordered, PrefixIdentifierIndex::withPrefix);
+
+  public static final RootSupplier EXACT_UNORDERED_ED_EXACT = new RootSupplier(
+      NodeIndex::withUnordered,
+      IdentifierIndex::withOnlyExact,
+      ExternalDeclarationIndex::withOnlyExact);
+  public static final RootSupplier PREFIX_UNORDERED_ED_PREFIX = new RootSupplier(
+      NodeIndex::withUnordered,
+      PrefixIdentifierIndex::withOnlyExact,
+      PrefixExternalDeclarationIndex::withOnlyExact);
 
   public static final RootSupplier EMPTY = new RootSupplier(supplier(null), supplier(null));
   public static final RootSupplier ONLY_NODE_INDEX = new RootSupplier(
